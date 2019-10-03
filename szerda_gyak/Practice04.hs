65d05f1a6fda892b7a2954fe35b05096eb32172b
anabra <peter.d.podlovics@gmail.com>
2019-10-03 10:34:31 +0200

    Added Practice04 - Wednesday

---
+++ szerda_gyak/Practice04.hs
@@ -0,0 +1,81 @@
+{-# LANGUAGE KindSignatures, InstanceSigs #-}
+module Practice04 where
+
+nameIdDB :: [(String, Int)]
+nameIdDB = [ ("John",    1)
+           , ("Richard", 2)
+           , ("Julie",   3)
+           ]
+
+data Currency = USD | GBP
+  deriving (Eq, Ord, Show)
+
+idMoneyCurDB :: [(Int, (Double, Currency))]
+idMoneyCurDB = [ (1, (11, USD))
+               , (5, (22, USD))
+               , (3, (33, GBP))
+               ]
+
+exchangeRateDB :: [(Currency, Double)]
+exchangeRateDB = [ (USD, 1)
+                 , (GBP, 1.1)
+                 ]
+
+getMoneyInUSD :: String -> Maybe Double 
+getMoneyInUSD name = case lookup name nameIdDB of 
+  Nothing -> Nothing
+  Just id -> case lookup id idMoneyCurDB of 
+    Nothing                 -> Nothing 
+    Just (amount, currency) -> case lookup currency exchangeRateDB of 
+      Nothing     -> Nothing 
+      Just exRate -> Just $ amount * exRate  
+
+getMoneyInUSD' :: String -> Maybe Double 
+getMoneyInUSD' name = do 
+  id                 <- lookup name nameIdDB
+  (amount, currency) <- lookup id idMoneyCurDB
+  exRate             <- lookup currency exchangeRateDB
+  pure $ amount * exRate
+
+getMoneyOfJulie :: Maybe Double 
+getMoneyOfJulie = do 
+  id                 <- lookup "Julie" nameIdDB
+  (amount, currency) <- lookup id idMoneyCurDB
+  exRate             <- lookup currency exchangeRateDB
+  pure $ amount * exRate
+
+class Monad' (m :: * -> *) where 
+  return :: a -> m a
+  (>>=)  :: m a -> (a -> m b) -> m b
+  
+instance Monad' Maybe where
+  return :: a -> Maybe a  
+  return x = Just x 
+
+  (>>=) :: Maybe a -> (a -> Maybe b) -> Maybe b
+  (>>=) m cont = case m of 
+    Nothing -> Nothing
+    Just x  -> cont x
+
+{-
+  id                 <- lookup name nameIdDB
+  (amount, currency) <- lookup id idMoneyCurDB
+  exRate             <- lookup currency exchangeRateDB
+  pure $ amount * exRate
+
+  DESUGARED INTO: 
+
+  lookup name nameIdDB >>= (\id ->
+  lookup id idMoneyCurDB >>= (\(amount, currency) -> 
+  lookup currency exchangeRateDB >>= (\exRate -> 
+  pure $ amount * exRate)))
+-}
+
+{-
+  x <- m 
+  <cont>
+
+  DESUGARED INTO:
+
+  m >>= (\x -> <cont>)
+-}
