bf268b21a5379785bbc8872523013e5b7c5df795
Podlovics Péter Dávid <anabra@INF.ELTE.HU>
2020-02-13 20:52:03 +0100

    Practice 1 1930 cont

--- 2019-20-2/gyak/podlovics/1930/Practice1.hs
+++ 2019-20-2/gyak/podlovics/1930/Practice1.hs
@@ -7,6 +7,9 @@ data Nat = Zero | Suc Nat
 zero :: Nat 
 zero = Zero 
 
+two :: Nat 
+two = Suc (Suc Zero)
+
 three :: Nat 
 three = Suc (Suc (Suc Zero))
 
@@ -20,12 +23,33 @@ eqNat (Suc n) (Suc m) = eqNat n m
 eqNat _ _ = False
 
 inc :: Nat -> Nat
-inc _ = undefined 
+inc = Suc
 
 addNat :: Nat -> Nat -> Nat 
-addNat _ _ = undefined 
+addNat Zero n    = n 
+addNat (Suc m) n = Suc (addNat m n) 
 
 mulNat :: Nat -> Nat -> Nat 
-mulNat _ _ = undefined
+mulNat Zero _    = Zero 
+mulNat (Suc m) n = addNat n (mulNat m n) 
+-- (m+1) * n = n + m*n
 
+ltNat :: Nat -> Nat -> Bool 
+ltNat _ Zero          = False 
+ltNat Zero _          = True 
+ltNat (Suc n) (Suc m) = ltNat n m
 
+-- listák ...
+
+data List a = Nil             -- []
+            | Cons a (List a) -- (:)
+  deriving Show
+
+lengthL :: List a -> Int
+lengthL Nil = 0
+lengthL (Cons x xs) = 1 + lengthL xs
+
+eqL :: Eq a => List a -> List a -> Bool
+eqL Nil Nil = True 
+eqL (Cons x xs) (Cons y ys) = x == y && eqL xs ys
+eqL _ _ = False
