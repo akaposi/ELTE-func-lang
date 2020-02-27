87c7cedc1de6d5e1f6bc8899b4e5ed90f2f6b70a
Podlovics Péter Dávid <anabra@INF.ELTE.HU>
2020-02-27 18:25:44 +0100

    Practice 1745 PRE 2

--- 2019-20-2/gyak_3/Practice3.hs
+++ 2019-20-2/gyak_3/Practice3.hs
@@ -3,12 +3,14 @@ module Practice3 where
 -- ex1.
 -- BinTree amiben a Node-ban vannak az értékek, Leaf-ben nem
 
-data BinTreeNode a = TODO
+data BinTreeNode a = TODO1
+  deriving (Eq, Show, Ord)
 
 -- ex2.
 -- minde levélben, mind csúcsokban, de eltérő típusú is lehet!
 
-data BinTreeBoth l n = TODO 
+data BinTreeBoth l n = TODO2
+  deriving (Eq, Show, Ord)
 
 concatTreeLeaves :: Semigroup l => BinTreeBoth l n -> l 
 concatTreeLeaves = undefined 
@@ -18,3 +20,17 @@ concatTreeNodes = undefined
 
 concatMapTree :: Monoid m => (l -> m) -> (n -> m) -> BinTreeBoth l n -> m 
 concatMapTree = undefined
+
+data T a = TodoT
+
+lift :: a -> T a 
+lift = undefined
+
+-- LAWS: lift (x <> y) == lift x <> lift y
+instance Semigroup (T a) where 
+  (<>) lhs rhs = undefined
+
+-- LAWS: lift x <> lift mempty == lift x
+--       lift mempty <> lift x == lift x
+instance Monoid (T a) where 
+  mempty = undefined
