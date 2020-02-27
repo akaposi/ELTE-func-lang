a705d557b484e86e3c25436d4d06a44a2a504a00
Podlovics Péter Dávid <anabra@INF.ELTE.HU>
2020-02-27 19:22:05 +0100

    Practice 1745 POST

--- 2019-20-2/gyak_3/Practice3.hs
+++ 2019-20-2/gyak_3/Practice3.hs
@@ -3,23 +3,27 @@ module Practice3 where
 -- ex1.
 -- BinTree amiben a Node-ban vannak az értékek, Leaf-ben nem
 
-data BinTreeNode a = TODO1
+data BinTreeNode a = Leaf' | Node' a (BinTreeNode a) (BinTreeNode a)
   deriving (Eq, Show, Ord)
 
 -- ex2.
--- minde levélben, mind csúcsokban, de eltérő típusú is lehet!
+-- mind levelekben, mind csúcsokban, de eltérő típusú is lehet!
 
-data BinTreeBoth l n = TODO2
+data BinTreeBoth l n = Leaf l | Node n (BinTreeBoth l n) (BinTreeBoth l n)
   deriving (Eq, Show, Ord)
 
 concatTreeLeaves :: Semigroup l => BinTreeBoth l n -> l 
-concatTreeLeaves = undefined 
+concatTreeLeaves (Leaf x) = x 
+concatTreeLeaves (Node _ lhs rhs) = concatTreeLeaves lhs <> concatTreeLeaves rhs 
 
 concatTreeNodes :: Monoid n => BinTreeBoth l n -> n 
-concatTreeNodes = undefined 
+concatTreeNodes (Leaf x) = mempty
+concatTreeNodes (Node y lhs rhs) = y <> concatTreeNodes lhs <> concatTreeNodes rhs 
 
 concatMapTree :: Monoid m => (l -> m) -> (n -> m) -> BinTreeBoth l n -> m 
-concatMapTree = undefined
+concatMapTree f g (Leaf x) = f x
+concatMapTree f g (Node y lhs rhs) = g y <> 
+  concatMapTree f g lhs <> concatMapTree f g rhs
 
 data T a = TodoT
 
@@ -30,7 +34,6 @@ lift = undefined
 instance Semigroup (T a) where 
   (<>) lhs rhs = undefined
 
--- LAWS: lift x <> lift mempty == lift x
---       lift mempty <> lift x == lift x
+-- LAWS: lift mempty == (mempty :: T a)
 instance Monoid (T a) where 
   mempty = undefined
