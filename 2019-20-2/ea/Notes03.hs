f189f1587d21ff0b3a611d9fbc9c9c73447ff4c1
András Kovács <puttamalac@gmail.com>
2020-02-27 15:29:50 +0100

    ea03 pre-notes

---
+++ 2019-20-2/ea/Notes03.hs
@@ -0,0 +1,15 @@
+
+-- Semigroup, Monoid, Functor
+------------------------------------------------------------
+
+-- Semigroup, Monoid
+-- list, maybe, newtype (Plus, Mult, Fun)
+-- laws
+
+-- Functor
+-- container: list, tree, maybe
+-- extra param: Either a, ((->) a), ((,) a)
+-- laws
+
+-- elemi:
+-- Id, Const, Comp, Prod, Sum
