ce4fe5d59d42ce3134cbd7615870498c6a5d59c8
anabra <peter.d.podlovics@gmail.com>
2020-03-26 18:26:15 +0100

    Add Practice5 1745 PRE 2

--- 2019-20-2/gyak_3/Practice5.hs
+++ 2019-20-2/gyak_3/Practice5.hs
@@ -11,8 +11,8 @@ module Practice5 where
 
 -- fmap :: (a -> b) -> f a -> f b
 
--- data Maybe' a = Nothing' | Just' a
---   deriving (Eq, Ord, Show)
+data Maybe' a = Nothing' | Just' a
+  deriving (Eq, Ord, Show)
 
 foo :: Eq k => [(k,v)] -> k -> Maybe v
 foo [] _ = Nothing
@@ -20,3 +20,64 @@ foo ((k,v):rest) toFind
   | k == toFind = Just v
   | otherwise   = foo rest toFind
 
+
+isJust :: Maybe a -> Bool
+isJust = undefined
+
+fromJust :: Maybe a -> a
+fromJust = undefined
+
+catMaybes :: [Maybe a] -> [a]
+catMaybes = undefined
+
+mapMaybe :: (a -> Maybe b) -> [a] -> [b]
+mapMaybe = undefined
+
+safeHead :: [a] -> Maybe a
+safeHead = undefined
+
+fmapMaybe :: (a -> b) -> Maybe a -> Maybe b
+fmapMaybe = undefined
+
+bindMaybe :: Maybe a -> (a -> Maybe b) -> Maybe b
+bindMaybe = undefined
+
+{- tests
+bindMaybe Nothing id                  == Nothing
+bindMaybe (Just 2) (\x -> Just (2*x)) == Just 4
+bindMaybe (Just 2) (\x -> Nothing)    == Nothing
+
+returnMaybe 5 == Just 5
+-}
+
+
+
+
+
+
+
+
+nameIdDB :: [(String, Int)]
+nameIdDB = [ ("John",    1)
+           , ("Richard", 2)
+           , ("Julie",   3)
+           ]
+
+data Currency = USD | GBP
+  deriving (Eq, Ord, Show)
+
+idMoneyCurDB :: [(Int, (Double, Currency))]
+idMoneyCurDB = [ (1, (11, USD))
+               , (5, (22, USD))
+               , (3, (33, GBP))
+               ]
+
+exchangeRateDB :: [(Currency, Double)]
+exchangeRateDB = [ (USD, 1)
+                 , (GBP, 1.1)
+                 ]
+
+-- lookup, foo
+
+lookupMoneyInUSD :: String -> Double
+lookupMoneyInUSD = undefined
