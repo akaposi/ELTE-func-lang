d77b1b426caf7635ba3df45dd42d08d5838286c0
András Kovács <puttamalac@gmail.com>
2020-05-07 15:47:34 +0200

    ea 11 pre

---
+++ 2019-20-2/ea/Notes11.hs
@@ -0,0 +1,58 @@
+{-# language DeriveFunctor #-}
+
+import Prelude hiding (exp)
+import Control.Monad
+import Control.Applicative    -- many, some
+import Data.Char -- isSpace, isLetter, isDigit, isAlpha
+
+newtype Parser a = Parser {runParser :: String -> Maybe (a, String)}
+  deriving Functor
+
+instance Applicative Parser where
+  pure = return
+  (<*>) = ap
+
+instance Monad Parser where
+  return a = Parser $ \s -> Just (a, s)
+  Parser f >>= g = Parser $ \s ->
+    case f s of
+      Nothing     -> Nothing
+      Just(a, s') -> runParser (g a) s'
+
+instance Alternative Parser where
+  empty = Parser $ \_ -> Nothing
+  (<|>) (Parser f) (Parser g) = Parser $ \s -> case f s of
+    Nothing -> g s
+    x       -> x
+
+satisfy :: (Char -> Bool) -> Parser Char
+satisfy f = Parser $ \s -> case s of
+  c:cs | f c -> Just (c, cs)
+  _          -> Nothing
+
+eof :: Parser ()
+eof = Parser $ \s -> case s of
+  [] -> Just ((), [])
+  _  -> Nothing
+
+char :: Char -> Parser ()
+char c = () <$ satisfy (==c)
+
+string :: String -> Parser ()
+string = mapM_ char
+
+ws :: Parser ()
+ws = () <$ many (char ' ' <|> char '\n')
+
+sepBy1 :: Parser a -> Parser b -> Parser [a]
+sepBy1 pa pb = (:) <$> pa <*> many (pb *> pa)
+
+sepBy :: Parser a -> Parser b -> Parser [a]
+sepBy pa pb = sepBy1 pa pb <|> pure []
+
+anyChar :: Parser Char
+anyChar = satisfy (const True)
+
+--------------------------------------------------------------------------------
+
+-- LC parse, kiértékelés closure-el, esetleg Reader-el
