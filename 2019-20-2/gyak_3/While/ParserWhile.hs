0eb73b50a2ed4a87fed2119c46d35e6f9490dd87
anabra <peter.d.podlovics@gmail.com>
2020-05-07 18:36:47 +0200

    Refine ParserWhile 1745 PRE

--- 2019-20-2/gyak_3/While/ParserWhile.hs
+++ 2019-20-2/gyak_3/While/ParserWhile.hs
@@ -17,20 +17,27 @@ bool = true <|> false
 bLit :: Parser Lit
 bLit = LBool <$> bool
 
--- TODO: use natural
 iLit :: Parser Lit
-iLit = undefined
+iLit = LInt <$> (natural <* ws)
 
 lit :: Parser Lit
 lit = bLit <|> iLit
 
--- TODO: use lowerAlpha (not token)
-var :: Parser Var
-var = undefined
+name :: Parser Name
+name = some lowerAlpha <* ws
 
--- TODO: ELit, EVar
+var :: Parser Var
+var = Var <$> name
+
+expr' :: Parser Expr
+expr' = EVar <$> var
+    <|> ELit <$> lit
+    <|> token "(" *> expr <* token ")"
+
 expr :: Parser Expr
-expr = undefined
+expr = Plus  <$> (expr' <* token "+") <*> expr
+   <|> Minus <$> (expr' <* token "-") <*> expr
+   <|> expr'
 
 statement :: Parser Statement
 statement = undefined
@@ -52,4 +59,27 @@ runParser var "x5" == Just (Var "x", "5")
 
 runParser expr "x"  == Just (EVar (Var "x"), "")
 runParser expr "53" == Just (ELit (LInt 53), "")
+-}
+
+{-
+"1 + 2 + 3" -> 1 + (2 + 3)
+(Plus
+   (ELit (LInt 1))
+   (Plus
+      (ELit (LInt 2))
+      (ELit (LInt 3)))
+
+"1 - 2 - 3" -> 1 - (2 - 3)
+(Minus
+   (ELit (LInt 1))
+   (Minus
+      (ELit (LInt 2))
+      (ELit (LInt 3)))
+
+"(1 - 2) - 3" -> (1 - 2) - 3
+(Minus
+   (Minus
+      (ELit (LInt 1))
+      (ELit (LInt 2)))
+   (ELit (LInt 3))
 -}
