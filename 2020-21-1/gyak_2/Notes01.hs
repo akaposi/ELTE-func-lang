b1fb73c958fce9acadc9e0706116e69d0ca03f66
Rafaël Bocquet <rafael.bocquet@ens.fr>
2020-09-16 19:17:31 +0200

    Update Notes01.hs

--- 2020-21-1/gyak_2/Notes01.hs
+++ 2020-21-1/gyak_2/Notes01.hs
@@ -8,14 +8,18 @@ module Notes01 where
 --  :i expr       Display information about the name `expr`
 
 -- Typed holes
---   (In vscode: press F8 to display the expected type of a hole, and the types of the variables in scope)
+--   (In vscode: press F8 to display the expected type of a hole,
+--               and the types of the variables in scope)
 --   (In GHCi: reload to see the same information)
 
 f0 :: ((a, b) -> c) -> b -> a -> c
-f0 = _
+-- f0 = undefined
+-- f0 g b a = _
+-- f0 g b a = g _
+-- f0 g b a = g (_ , _)
+f0 g b a = g (a, b)
 
-f1 :: (a -> b) -> a -> (b, a)
-f1 = _
+-- f1 :: _
 
 -- Algebraic data types
 
@@ -25,7 +29,6 @@ data Pair a b = Pair a b
 -- Either a b in Prelude
 data Either' a b = Left' a
                  | Right' b
-                 deriving (Show)
 
 -- Maybe a in Prelude
 data Maybe' a = Just' a 
@@ -43,35 +46,52 @@ data BinTree a = Leaf a
 
 -- Show, Eq and Ord instances
 
+-- class Show a where
+--   show :: a -> String
+
+-- class Eq a where
+--   (==) :: a -> a -> Bool
+
+-- class Eq a => Ord a where
+--   (<=) :: a -> a -> Bool
+
 instance (Show a, Show b) => Show (Pair a b) where
-  show x = undefined
+  show (Pair a b) = "(" ++ show a ++ ", " ++ show b ++ ")"
 
 instance (Eq a, Eq b) => Eq (Pair a b) where
-  x == y = undefined
+  Pair x1 x2 == Pair y1 y2 = (x1 == y1) && (x2 == y2)
+
+instance (Ord a, Ord b) => Ord (Pair a b) where
+  Pair x1 x2 <= Pair y1 y2
+    | x1 == y1  = x2 <= y2
+    | otherwise = x1 < y1
+
+instance (Show a, Show b) => Show (Either' a b) where
+  show (Left' x) = "(Left' " ++ show x ++ ")"
+  show (Right' x) = "(Right' " ++ show x ++ ")"
 
 instance (Eq a, Eq b) => Eq (Either' a b) where
-  x == y = undefined
+  Left' x  == Left' y  = (x == y)
+  Right' x == Right' y = (x == y)
+  _        == _        = False
+
+instance (Ord a, Ord b) => Ord (Either' a b) where
+  Left' x  <= Left' y  = (x <= y)
+  Left' x  <= _        = True
+  _        <= Left' _  = False
+  Right' x <= Right' y = (x <= y)
 
 instance Eq a => Eq (Maybe' a) where
-  x == y = undefined
+  Nothing' == Nothing' = True
+  Just' x  == Just' y  = x == y
+  _        == _        = False
 
 instance Eq a => Eq (List a) where
-  x == y = undefined
+  Empty     == Empty     = True
+  Cons x xs == Cons y ys = (x == y) && (xs == ys)
+  _         == _         = False
 
 instance Eq a => Eq (BinTree a) where
-  x == y = undefined
-
-instance (Show a, Show b) => Show (Pair a b) where
-  show x = undefined
-
-
--- map in Prelude
-mapList :: (a -> b) -> List a -> List b
-mapList f Empty       = undefined
-mapList f (Cons x xs) = undefined
-
-mapMaybe :: (a -> b) -> Maybe' a -> Maybe' b
-mapMaybe x y = undefined
-
-mapTree :: (a -> b) -> BinTree a -> BinTree b
-mapTree = undefined
+  Leaf x     == Leaf y     = x == y
+  Node x1 x2 == Node y1 y2 = (x1 == y1) && (x2 == y2)
+  _          == _          = False
