0ee0feeeef9b6ce8c313b0367f9f018569b63fb2
Rafaël Bocquet <rafael.bocquet@ens.fr>
2020-10-21 19:43:14 +0200

    Upd gyak_2/Notes07.hs

--- 2020-21-1/gyak_2/Notes07.hs
+++ 2020-21-1/gyak_2/Notes07.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE DeriveFunctor, MonadComprehensions #-}
+{-# LANGUAGE DeriveFunctor, DeriveFoldable, DeriveTraversable, MonadComprehensions #-}
 module Notes07 where
 
 import Control.Monad (ap, forM, forM_, liftM2)
@@ -6,18 +6,27 @@ import Control.Monad.State
 
 -------------------------------------------------------------------------------
 
+data BinTree a = Leaf a | Node (BinTree a) (BinTree a)
+  deriving (Functor, Foldable, Traversable)
+
 -- class Semigroup m where
 --   (<>) :: m -> m -> m
 
--- class Monoid m where
+-- class Semigroup m => Monoid m where
 --   mempty :: m
 
 -- instance Semigroup [] where (<>) = (++)
 -- instance Monoid [] where mempty = []
 
 -- class Foldable f where
---   foldMap :: Monoid m => (m -> a) -> f a -> m
+--   foldMap :: Monoid m => (a -> m) -> f a -> m
 --   foldr   :: (a -> b -> b) -> b -> f a -> b
+
+foldMapList :: Monoid m => (a -> m) -> [a] -> m
+foldMapList f []     = mempty
+foldMapList f (x:xs) = f x <> foldMapList f xs
+
+-- foldMapList f [x, y, z] == f x <> f y <> f z <> mempty == f x <> f y <> f z
 
 -- Define foldMap using foldr
 foldMapFromFoldr :: (Foldable f, Monoid m) => (m -> a) -> f a -> m
@@ -34,6 +43,7 @@ toList' :: Foldable f => f a -> [a]
 toList' = undefined
 
 -- Define the functions null'', length'' and toList'' using foldMap
+-- Hint : you should define some monoid instances for Bool and Int
 null'' :: Foldable f => f a -> Bool
 null'' = undefined
 
@@ -41,7 +51,7 @@ length'' :: Foldable f => f a -> Int
 length'' = undefined
 
 toList'' :: Foldable f => f a -> [a]
-toList'' = undefined
+toList'' = foldMap (\x -> [x])
 
 -- class Traversable f where
 --   traverse :: Applicative m => (a -> m b) -> f a -> m (f b)
