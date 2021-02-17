3b163cf8f5e308267936d5b08f3890b52f57ee3f
Rafaël Bocquet <rafael.bocquet@ens.fr>
2021-02-17 19:24:01 +0100

    Update gyak_4/Notes02.hs

--- 2020-21-2/gyak_4/Notes02.hs
+++ 2020-21-2/gyak_4/Notes02.hs
@@ -39,38 +39,68 @@ instance Ord' Color where
 
   compare' Green Green = EQ
 
+instance Ord' Integer where compare' = compare
+instance Ord' Int where compare' = compare
+instance Ord' Char where compare' = compare
+
+lexi :: Ordering -> Ordering -> Ordering
+lexi EQ y = y
+lexi x  y = x
+
+-- This is the same as (<>) :: Ordering -> Ordering -> Ordering in the Prelude.
+
+
+-- Lexicographic order
 instance (Ord' a, Ord' b) => Ord' (a, b) where
   compare' :: (a, b) -> (a, b) -> Ordering
-  compare' = undefined
+  -- compare' (a1, b1) (a2, b2) = case compare' a1 a2 of
+  --   EQ -> compare' b1 b2
+  --   x  -> x
+  compare' (a1, b1) (a2, b2) = compare' a1 a2 `lexi` compare' b1 b2
 
 instance (Ord' a, Ord' b) => Ord' (Either a b) where
   compare' :: Either a b -> Either a b -> Ordering
-  compare' = undefined
+  compare' (Left x) (Left y) = compare' x y
+  compare' (Left _) _        = LT
+  compare' _        (Left _) = GT
+
+  compare' (Right x) (Right y) = compare' x y
 
 instance Ord' a => Ord' [a] where
   compare' :: [a] -> [a] -> Ordering
-  compare' = undefined
+  compare' [] [] = EQ
+  compare' [] _  = LT
+  compare' _  [] = GT
+
+  compare' (x : xs) (y : ys) = compare' x y `lexi` compare' xs ys
 
 instance Ord' a => Ord' (BinTree a) where
   compare' :: BinTree a -> BinTree a -> Ordering
-  compare' = undefined
+  compare' (Leaf x) (Leaf y) = compare' x y
+  compare' (Leaf _) _        = LT
+  compare' _        (Leaf _) = GT
+
+  compare' (Node l1 r1) (Node l2 r2) = compare' l1 l2 `lexi` compare' r1 r2
 
 --------------------------------------------------------------------------------
 -- You know the function map on lists:
 
 map' :: (a -> b) -> [a] -> [b]
-map' f xs = undefined
+map' f []     = []
+map' f (x:xs) = f x : map' f xs
 
 -- Define mapping functions for other types:
 mapMaybe :: (a -> b) -> Maybe a -> Maybe b
-mapMaybe = undefined
+mapMaybe f Nothing  = Nothing
+mapMaybe f (Just x) = Just (f x)
 
 mapPair :: (a -> b) -> (c -> d) -> (a, c) -> (b, d)
-mapPair = undefined
+mapPair f g (a, c) = (f a, g c)
 
-mapEither :: (a -> b) -> (c -> d) -> (a, c) -> (b, d)
-mapEither = undefined
+mapEither :: (a -> b) -> (c -> d) -> Either a c -> Either b d
+mapEither f g (Left a)  = Left (f a)
+mapEither f g (Right c) = Right (g c)
 
 mapBinTree :: (a -> b) -> BinTree a -> BinTree b
-mapBinTree = undefined
-
+mapBinTree f (Leaf x)   = Leaf (f x)
+mapBinTree f (Node l r) = Node (mapBinTree f l) (mapBinTree f r)
