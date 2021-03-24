88d41930999d6056bdd7bfaa85761a527cce4113
Rafaël Bocquet <rafael.bocquet@ens.fr>
2021-03-24 19:52:17 +0100

    Update gyak_4/Notes07.hs

--- 2020-21-2/gyak_4/Notes07.hs
+++ 2020-21-2/gyak_4/Notes07.hs
@@ -36,7 +36,6 @@ execState :: State s a -> s -> s
 execState ma = snd . runState ma
 
 --------------------------------------------------------------------------------
-
 -- Labelling trees using the State monad
 
 data BinTree a = Leaf a 
@@ -52,23 +51,40 @@ data BinTree a = Leaf a
 -- Hint: define a function labelTree_State :: BinTree a -> State Int (BinTree Int), 
 --   where the state represents the next leaf value.
 -- labelTree_State should be defined by recursion on its argument.
-labelTree_State :: BinTree a -> State Int (BinTree Int)
-labelTree_State = undefined
+labelTreeState :: BinTree a -> State Int (BinTree Int)
+labelTreeState (Leaf _)   = do
+  i <- get
+  put (i+1)
+  pure (Leaf i)
+labelTreeState (Node l r) = do
+  l' <- labelTreeState l
+  r' <- labelTreeState r
+  pure (Node l' r')
+-- labelTreeState (Node l r) = Node <$> labelTreeState l <*> labelTreeState r
 
 -- labelTree should be defined using evalState and labelTree_State
 labelTree :: BinTree a -> BinTree Int
-labelTree = undefined
+labelTree t = evalState (labelTreeState t) 0
 
 -- The function labelTreeMax should label the leaves of a tree with the maximum leaf value 
 --        to the left of it (you can assume that all values are positive).
 --    labelTreeMax (Leaf 10) == Leaf 10
 --    labelTreeMax (Node (Leaf 10) (Leaf 100)) == Node (Leaf 10) (Leaf 100)
 --    labelTreeMax (Node (Leaf 100) (Leaf 10)) == Node (Leaf 100) (Leaf 100)
---    labelTreeMax (Node (Leaf 2) (Node (Leaf 1) (Leaf 3))) == Node (Leaf 2) (Node (Leaf 2) (Node Leaf 3))
+--    labelTreeMax (Node (Leaf 2) (Node (Leaf 1) (Leaf 3))) == Node (Leaf 2) (Node (Leaf 2) (Leaf 3))
 --    ..
 
+labelTreeMaxState :: BinTree Int -> State Int (BinTree Int)
+labelTreeMaxState (Leaf x)   = do
+  modify (max x)
+  Leaf <$> get
+labelTreeMaxState (Node l r) = do
+  l' <- labelTreeMaxState l
+  r' <- labelTreeMaxState r
+  pure (Node l' r')
+
 labelTreeMax :: BinTree Int -> BinTree Int
-labelTreeMax = undefined
+labelTreeMax t = evalState (labelTreeMaxState t) 0
 
 
 --------------------------------------------------------------------------------
@@ -77,6 +93,7 @@ labelTreeMax = undefined
 -- Generalizations of `fmap :: Functor f => (a -> b) -> f a -> f b`:
 
 -- foldMap :: (Foldable t, Monoid m) => (a -> m) -> t a -> m
+
 -- mapM    :: (Traversable t, Monad m) => (a -> m b) -> t a -> m (t b)
 -- More general: traverse :: (Traversable t, Applicative m) => (a -> m b) -> t a -> m (t b)
 
@@ -91,24 +108,43 @@ traverse_List f (x:xs) = (:) <$> f x <*> traverse_List f xs
 
 -- Define foldMap and mapM/traverse for BinTree
 
-foldMap_BinTree :: Monoid m => (a -> m) -> BinTree a -> m
-foldMap_BinTree = undefined
+foldMapBinTree :: Monoid m => (a -> m) -> BinTree a -> m
+foldMapBinTree f (Leaf x)   = f x
+foldMapBinTree f (Node l r) = foldMapBinTree f l <> foldMapBinTree f r
 
-mapM_BinTree :: Monad m => (a -> m b) -> BinTree a -> m (BinTree b)
-mapM_BinTree = undefined
+traverseBinTree :: Applicative m => (a -> m b) -> BinTree a -> m (BinTree b)
+traverseBinTree f (Leaf x)   = Leaf <$> f x
+traverseBinTree f (Node l r) = Node <$> traverseBinTree f l <*> traverseBinTree f r
 
-traverse_BinTree :: Applicative m => (a -> m b) -> BinTree a -> m (BinTree b)
-traverse_BinTree = undefined
+mapMBinTree :: Monad m => (a -> m b) -> BinTree a -> m (BinTree b)
+mapMBinTree = traverseBinTree
+
+labelTree' :: BinTree a -> BinTree Int
+labelTree' t = evalState (traverseBinTree go t) 0
+  where go _ = do i <- get
+                  put (i+1)
+                  pure i
 
 instance Foldable BinTree where 
-  foldMap = foldMap_BinTree
+  foldMap = foldMapBinTree
 instance Traversable BinTree where 
-  traverse = traverse_BinTree
+  traverse = traverseBinTree
+
 
 -- Bonus: Define fmap and foldMap using traverse.
 
+data Id a = Id { getId :: a}
+instance Functor Id where fmap f (Id a) = Id (f a)
+instance Applicative Id where pure = Id; Id f <*> Id a = Id (f a)
+
 fmap' :: Traversable f => (a -> b) -> f a -> f b
-fmap' = undefined
+fmap' f t = getId (traverse (Id . f) t)
+
+data Writer m a = Writer { getWriter :: m }
+instance Functor (Writer m) where fmap f (Writer x) = Writer x
+instance Monoid m => Applicative (Writer m) where 
+  pure _ = Writer mempty
+  Writer a <*> Writer b = Writer (a <> b)
 
 foldMap' :: (Traversable f, Monoid m) => (a -> m) -> f a -> m
-foldMap' = undefined
+foldMap'  t = getWriter (traverse (Writer . f) t)
