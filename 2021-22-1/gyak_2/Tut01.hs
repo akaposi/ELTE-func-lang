47cd784d9a693ae6f505189af8eb76212dd9029a
Rafaël Bocquet <rbocquet@localhost.localdomain>
2021-09-16 17:46:19 +0200

    Add gyak_3/Tut02.hs

--- 2021-22-1/gyak_2/Tut01.hs
+++ 2021-22-1/gyak_2/Tut01.hs
@@ -2,29 +2,43 @@
 module Tut01 where
 import Prelude hiding (Eq(..), Ord(..))
 
--- This file: https://github.com/AndrasKovacs/ELTE-func-lang/tree/master/2021-22-1/gyak_2/Notes01.hs
+-- This file: https://github.com/AndrasKovacs/ELTE-func-lang/tree/master/2021-22-1/gyak_2/Tut01.hs
 
 ----- General information:
 -- https://github.com/AndrasKovacs/ELTE-func-lang/tree/master/2021-22-1
 
 ----- Holes:
+-- Reload in GHCI or press F8 in Visual Studio code to see the type of a hole `_`
 
 ex0 :: a -> b -> a
-ex0 = _
+ex0 x y = _
 
 ----- Common Algebraic Datatypes: (,), Either, Maybe
 
 -- data Maybe a = Nothing 
 --              | Just a
 
+-- product types
 -- data (,) a b = (,) a b
+--      (a,b)
 
+--     (x,y) :: (a,b)
+--   when x :: a
+--        y :: b
+
+-- sum types
 -- data Either a b = Left a
 --                 | Right b
 
+-- Left x :: Either a b
+--   when x :: a
+-- Right x :: Either a b
+--   when x :: b
+
 data BinTree a = Leaf a
                | Node (BinTree a) (BinTree a)
-               deriving (Show) -- <-- this defines `show :: Show a => BinTree a -> String` automatically. 
+               deriving (Show) 
+ -- <-- this defines `show :: Show a => BinTree a -> String` automatically. 
 
 data Tree1 a = Leaf1 a
              | Node1 [Tree1 a]
@@ -32,64 +46,61 @@ data Tree1 a = Leaf1 a
 
 ----- Exercises: define functions f1 .. f11 with the given types.
 
+-- fst :: (a,b) -> a
+-- snd :: (a,b) -> b
 f1 :: (a, (b, (c, d))) -> (b, c)
-f1 = undefined
+f1 (x,(y,(z,w))) = (y , z)
 
 f2 :: (a -> b) -> a -> b
-f2 = undefined
+f2 f x = f x
+-- f2 f = f
 
 f3 :: (b -> c) -> (a -> b) -> a -> c
-f3 = undefined
+f3 f g x = f (g x)
 
-f4 :: (a -> b -> c) -> (b -> a -> c)
-f4 = undefined
+f4    :: (a -> b -> c) -> (b -> a -> c)
+-- f4 :: (a -> b -> c) -> b -> a -> c
+f4 f x y = f y x
 
-f5 :: ((a, b) -> c) -> (a -> b -> c)
-f5 = undefined
+f5    :: ((a, b) -> c) -> (a -> b -> c)
+-- f5 :: ((a, b) -> c) -> a -> b -> c
+f5 f x y = f (x , y)
+-- f5 = curry
+-- uncurry :: (a -> b -> c) -> ((a, b) -> c)
 
 f6 :: (a -> (b, c)) -> (a -> b, a -> c)
-f6 = undefined
+-- f6 f = (h1 , h2)
+--   where -- h1 :: a -> b
+--         h1 x = fst (f x)
+--             -- f x :: (b , c)
+--             -- fst (f x) :: b
+--         -- h2 :: a -> c
+--         h2 x = snd (f x)
+
+f6 f = (h1 , h2)
+  where h1 = \x -> fst (f x)
+        h2 = \x -> snd (f x)
+
+-- f6 f = (\x -> fst (f x), \x -> snd (f x))
 
 f7 :: (a -> b, a -> c) -> (a -> (b, c))
-f7 = undefined
+f7 (f,g) x = (f x, g x)
 
 f8 :: (Either a b -> c) -> (a -> c, b -> c)
-f8 = undefined
+f8 g = (h1, h2)
+  where h1 x = g (Left x)
+        h2 x = g (Right x)
+        -- Left  :: a -> Either a b
+        -- Right :: b -> Either a b
 
 f9 :: (a -> c, b -> c) -> (Either a b -> c)
-f9 = undefined
+f9 (f1, f2) (Left x)  = f1 x
+f9 (f1, f2) (Right y) = f2 y
 
 f10 :: Either (a, b) (a, c) -> (a, Either b c)
-f10 = undefined
+f10 (Left (x,y)) = (x, Left y)
+f10 (Right (x,y)) = (x, Right y)
 
 f11 :: (a, Either b c) -> Either (a, b) (a, c)
-f11 = undefined
-
------ The Eq and Ord typeclasses:
-
--- ghci> :i Eq
-class Eq a where
-  (==) :: a -> a -> Bool
-
-instance Eq Bool where
-  (==) = undefined
-
-instance Eq a => Eq (Maybe a) where
-  (==) = undefined
-
-instance (Eq a, Eq b) => Eq (Either a b) where
-  (==) = undefined
-
-instance (Eq a, Eq b) => Eq (a, b) where
-  (==) = undefined
-
-
--- ghci> :i Ord
-class Ord a where
-  (<=) :: a -> a -> Bool
-
-instance Ord Bool where
-  (<=) = undefined
-
-instance Ord a => Ord (Maybe a) where
-  (<=) = undefined
+f11 (x, Left y) = Left (x,y)
+f11 (x, Right y) = Right (x,y)
--- 2021-22-1/gyak_3/Tut01.hs
+++ 2021-22-1/gyak_3/Tut01.hs
@@ -2,15 +2,16 @@
 module Tut01 where
 import Prelude hiding (Eq(..), Ord(..))
 
--- This file: https://github.com/AndrasKovacs/ELTE-func-lang/tree/master/2021-22-1/gyak_3/Notes01.hs
+-- This file: https://github.com/AndrasKovacs/ELTE-func-lang/tree/master/2021-22-1/gyak_3/Tut01.hs
 
 ----- General information:
 -- https://github.com/AndrasKovacs/ELTE-func-lang/tree/master/2021-22-1
 
 ----- Holes:
+-- Reload in GHCI or press F8 in Visual Studio code to see the type of a hole `_`
 
 ex0 :: a -> b -> a
-ex0 = _
+ex0 x y = _
 
 ----- Common Algebraic Datatypes: (,), Either, Maybe
 
@@ -24,7 +25,8 @@ ex0 = _
 
 data BinTree a = Leaf a
                | Node (BinTree a) (BinTree a)
-               deriving (Show) -- <-- this defines `show :: Show a => BinTree a -> String` automatically. 
+               deriving (Show) 
+      -- <-- this defines `show :: Show a => BinTree a -> String` automatically. 
 
 data Tree1 a = Leaf1 a
              | Node1 [Tree1 a]
@@ -33,63 +35,52 @@ data Tree1 a = Leaf1 a
 ----- Exercises: define functions f1 .. f11 with the given types.
 
 f1 :: (a, (b, (c, d))) -> (b, c)
-f1 = undefined
+f1 (x,(y,(z,w))) = (y , z)
 
 f2 :: (a -> b) -> a -> b
-f2 = undefined
+f2 f y = f y
 
 f3 :: (b -> c) -> (a -> b) -> a -> c
-f3 = undefined
+-- f3 g h x = g (h x)
+f3 g h x = let y = h x 
+           in g y
+  -- x       :: a
+  -- h x     :: b   (because h :: a -> b)
+  -- g (h x) :: c   (because g :: b -> c)
+
+  -- ? g (h x) :: c 
 
 f4 :: (a -> b -> c) -> (b -> a -> c)
-f4 = undefined
+-- f4 :: (a -> b -> c) -> b -> a -> c
+f4 g x y = g y x
 
 f5 :: ((a, b) -> c) -> (a -> b -> c)
-f5 = undefined
+f5 f x y = f (x,y)
 
 f6 :: (a -> (b, c)) -> (a -> b, a -> c)
-f6 = undefined
+-- f6 f = (h1, h2)
+--   where h1 x = fst (f x)
+--         h2 x = snd (f x)
+f6 f = (\x -> fst (f x), \x -> snd (f x))
+        -- x :: a
+        -- f x :: (b, c)
+        -- fst (f x) :: b
+        -- snd (f x) :: c
 
 f7 :: (a -> b, a -> c) -> (a -> (b, c))
-f7 = undefined
+f7 (f,g) a = (f a, g a)
 
 f8 :: (Either a b -> c) -> (a -> c, b -> c)
-f8 = undefined
+f8 f = (\x -> f (Left x), \x -> f (Right x))
 
 f9 :: (a -> c, b -> c) -> (Either a b -> c)
-f9 = undefined
+f9 (f,g) (Left x) = f x
+f9 (f,g) (Right x) = g x
 
 f10 :: Either (a, b) (a, c) -> (a, Either b c)
-f10 = undefined
+f10 (Left (x,y)) = (x, Left y)
+f10 (Right (x,y)) = (x, Right y)
 
 f11 :: (a, Either b c) -> Either (a, b) (a, c)
-f11 = undefined
-
------ The Eq and Ord typeclasses:
-
--- ghci> :i Eq
-class Eq a where
-  (==) :: a -> a -> Bool
-
-instance Eq Bool where
-  (==) = undefined
-
-instance Eq a => Eq (Maybe a) where
-  (==) = undefined
-
-instance (Eq a, Eq b) => Eq (Either a b) where
-  (==) = undefined
-
-instance (Eq a, Eq b) => Eq (a, b) where
-  (==) = undefined
-
-
--- ghci> :i Ord
-class Ord a where
-  (<=) :: a -> a -> Bool
-
-instance Ord Bool where
-  (<=) = undefined
-
-instance Ord a => Ord (Maybe a) where
-  (<=) = undefined
+f11 (x, Left y) = Left (x,y)
+f11 (x, Right y) = Right (x,y)
