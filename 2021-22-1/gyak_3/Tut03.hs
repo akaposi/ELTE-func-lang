ef72484549b6f7458bb9c030fa6f9d757d643ab1
AndrasKovacs <puttamalac@gmail.com>
2021-09-25 08:59:25 +0200

    gyak 03

--- 2021-22-1/gyak_3/Tut03.hs
+++ 2021-22-1/gyak_3/Tut03.hs
@@ -1,80 +1,61 @@
 
-{-# language InstanceSigs #-}
+{-# language InstanceSigs #-}     -- nyelvi opciók
+                                  -- instance metódus típusannotáció
 
--- pragma: ghc options a fájl tetején
--- {-# language <nyelvi opció 1> <nyelvi opció 2> ... #-}
--- InstanceSig
-
--- Ajánlott option: figyelmeztet, ha nem fedjük le az összes esetet
--- mintaillesztésnél
+-- részleges mintaillesztésre figyelmeztet
 {-# options_ghc -Wincomplete-patterns #-}
-
--- Következő gyak:
---   - Felvételt csinálok
---   - Monad (lásd előadás)
---   - Óra elei feladat témája: egyszerűbb data-ra Functor instance
 
 ------------------------------------------------------------
 
 import Prelude hiding (Either(..), Functor(..), Semigroup(..), Monoid(..))
 
--- óra eleji feladat megoldása:
+-- részleges minta warning:
+-- foo :: Bool -> Bool
+-- foo True = False
 
-maybeSum :: [Maybe Int] -> Int
-maybeSum []            = 0
-maybeSum (Nothing:mns) = maybeSum mns
-maybeSum (Just n:mns)  = n + maybeSum mns
+------------------------------------------------------------
 
-maybeSum2 :: [Maybe Int] -> Int
-maybeSum2 []       = 0
-maybeSum2 (mn:mns) = case mn of
-  Nothing -> maybeSum2 mns
-  Just n  -> maybeSum2 mns + n
+sumMaybe :: [Maybe Int] -> Int
+sumMaybe []            = 0
+sumMaybe (Nothing:mis) = sumMaybe mis
+sumMaybe (Just n :mis) = n + sumMaybe mis
 
--- maybeSum2 (mn:mns) =
---   (case mn of Nothing -> 0; Just n -> n) + maybeSum2 mns
+--
+sumMaybe2 :: [Maybe Int] -> Int
+sumMaybe2 []       = 0
+sumMaybe2 (mi:mis) = case mi of
+  Nothing -> sumMaybe mis
+  Just n  -> n + sumMaybe mis
 
--- std függvény: maybe
--- maybeSum2 (mn:mns) = maybe 0 id mn + maybeSum2 mns
+-- case-ek 1 sorba írva: ;-el elválasztva:
+-- sumMaybe2 (mi:mis) =
+--   (case mi of Nothing -> 0; Just n -> n) + sumMaybe mis
 
+------------------------------------------------------------
 
---------------------------------------------------------------------------------
 
 data Tree a = Leaf a | Node (Tree a) (Tree a)
 
 class Eq' a where
   eq :: a -> a -> Bool
 
-class Eq' a => Ord' a where
-  lte :: a -> a -> Bool      -- (<=)
-
 class Show' a where
-  show' :: a -> String       --
+  show' :: a -> String
+
 
 instance Eq' a => Eq' (Tree a) where
   eq :: Tree a -> Tree a -> Bool
-  eq (Leaf a)   (Leaf a')    = eq a a'     -- Eq' a constraint-ből
+  eq (Leaf a)   (Leaf a')    = eq a a'
   eq (Node l r) (Node l' r') = eq l l' && eq r r'
   eq _          _            = False
 
--- "lexikografikus": Leaf _ <= Node _ _
-instance Ord' a => Ord' (Tree a) where
-  lte (Leaf _) (Node _ _)     = True
-  lte (Leaf a) (Leaf a')      = lte a a'
-  lte (Node l r) (Node l' r') = lte l l' && lte r r'
-  lte _          _            = False
-
-
--- -- emlékezzünk: (++) lista összefűzés, String összefűzés
--- instance Show' a => Show' (Tree a) where
---   show' (Leaf a) = "Leaf " ++ show' a
---   -- ...
-
--- Eq, Ord, Show
-
+instance Show' a => Show' (Tree a) where
+  show' = undefined
 
 -- Semigroup, Monoid, Functor
 ------------------------------------------------------------
+
+-- Előadásról: Semigroup, Monoid, Functor
 
 infixr 6 <>
 class Semigroup a where
@@ -85,6 +66,13 @@ class Semigroup a => Monoid a where
   mempty :: a           -- Semigroup + egységelem
                         -- mempty <> x = x
                         -- x <> mempty = x
+
+-- standard:
+-- instance Semigroup [a] where
+--   (<>) = (++)
+
+-- instance Monoid [a] where
+--   mempty = []
 
 class Functor f where
   fmap :: (a -> b) -> f a -> f b
@@ -100,23 +88,20 @@ instance Monoid [a] where
 
 instance (Semigroup a, Semigroup b) => Semigroup (a, b) where
   (<>) :: (a, b) -> (a, b) -> (a, b)
-  (a, b) <> (a', b') = (a <> a', b <> b')
+  (<>) = undefined
 
 instance (Monoid a, Monoid b) => Monoid (a, b) where
   mempty :: (a, b)
-  mempty = (mempty, mempty)
-  -- (x, y) <> (mempty, mempty) = (x <> mempty, y <> mempty) = (x, y)  OK
+  mempty = undefined
 
--- tipp: használjuk a _-t arra, hogy megkapjuk a metódusok típusát
 instance Semigroup b => Semigroup (a -> b) where
 
-  -- használjuk Semigroup b instance-t
   (<>) :: (a -> b) -> (a -> b) -> a -> b
-  (<>) f g a = f a <> g a
+  (<>) = undefined
 
 instance Monoid b => Monoid (a -> b) where
   mempty :: a -> b
-  mempty a = mempty
+  mempty = undefined
 
 --------------------------------------------------------------------------------
 
@@ -124,17 +109,19 @@ instance Monoid b => Monoid (a -> b) where
 
 -- általánosítás:
 -- class Functor f where
---   fmap :: (a -> b) -> f a -> f b
+--   fmap :: (a -> b) -> f a -> f b     -- "f" 1-paraméteres típus!
+
+-- instance Functor Maybe
 
 -- instance Functor [] where
---    fmap :: (a -> b) -> [] a -> [] b
+--    fmap :: (a -> b) -> [] a -> [] b    -- Maybe Int     [] Int    [Int]
 --    fmap = map
 
 --    fmap :: (a -> b) -> [a] -> [b]    -- szintaktikus cukorka lista típusra!
 --    fmap = map
 
 -- min 1 paraméter
---  utolsú típusparaméter fölött map-elünk (Functor instance-al)
+--  utolsó típusparaméter fölött map-elünk (Functor instance-al)
 
 data    Foo1 a      = Foo1 Int a a a deriving Show
 data    Foo2 a      = Foo2 Bool a Bool
@@ -148,12 +135,12 @@ newtype Id a        = Id a
 newtype Const a b   = Const a
 newtype Fun a b     = Fun (a -> b)
 
-instance Functor Foo1 where
-  fmap :: (a -> b) -> Foo1 a -> Foo1 b
-  fmap f (Foo1 n x y z) = Foo1 n (f x) (f y) (f z)
+               -- 4 mező, a típusú mezőt fmap-eljük!
+-- data Foo1 a = Foo1 Int a a a deriving Show
 
--- fmap (++"foo") $ Foo1 10 "a" "b" "c"
---   == Foo1 10 "afoo" "bfoo" "cfoo"
+instance Functor Foo1 where
+  fmap :: (a -> b) -> Foo1 a -> Foo1 b       -- InstanceSigs
+  fmap f (Foo1 n x y z) = Foo1 n (f x) (f y) (f z)
 
 instance Functor Foo2 where
   fmap = undefined
@@ -164,24 +151,63 @@ instance Functor Foo3 where
 instance Functor Tree1 where
   fmap = undefined
 
--- data Pair a b = Pair a b        (,)
--- Pair-t parciálisan alkalmazzuk 1 darab típusra
+-- (Bool, Bool)
+-- (True, False)
+
+-- data Pair a b = Pair a b
+--   Pair Int Int  : konkrét típus
+--   Pair Int      : 1-paraméteres típus
+
 instance Functor (Pair c) where
   fmap :: (a -> b) -> Pair c a -> Pair c b
   fmap f (Pair c a) = Pair c (f a)
 
-instance Functor Tree2 where
-  fmap = undefined
+-- data Tree2 a = Node2 a [Tree2 a] deriving Show
 
---      Tree4 Bool a         Node3 (Bool -> Tree Bool a)
---                           Node3 (Tree Bool a, Tree Bool a)
+t1' :: Tree2 Int                          -- Node x [] : "levél"
+t1' = Node2 10 [ Node2 12 [], Node2 20 [] ]
+
+-- általánosan: rekurzív típusban egy másik típust használok
+
+instance Functor Tree2 where
+  fmap :: (a -> b) -> Tree2 a -> Tree2 b
+  fmap f (Node2 a ts) = Node2 (f a) (map (fmap f) ts)
+     -- _    :: [Tree2 b]
+     -- f    :: a -> b
+     -- ts   :: [Tree2 a]
+     -- fmap :: (a -> b) -> Tree2 a -> Tree2 b
+
+  -- Mivel Tree2-ben listára hivatkozunk rekurzívan
+  -- rekurzív függvényt írok Tree2-n, a listákra vonatkozó függvényt használom fel
+
 -- data Tree3 i a = Leaf3 a | Node3 (i -> Tree3 i a)  -- i-szeres elágazás
+
+-- Tree3 Bool a
+-- Node3 :: (Bool -> Tree3 Bool a) -> Tree3 Bool a
+--          (Tree3 Bool a, Tree3 Bool a) -> Tree3 Bool a
+--  i = Bool --> bináris fa
+--  i = 3-elemű típus --> ternáris fa
+--  i = ()            --> unáris
+--  i = Int           --> 2^64 elágazású fa
+
+t1 :: Tree3 Bool Int
+t1 = Node3 (\i -> if i then Leaf3 10 else Leaf3 20)
+
+t2 :: Tree3 Int Int
+t2 = Node3 (\i -> Leaf3 i)
+   -- i-edik részfa definíciója: Leaf i
+
+t3 :: Tree3 Int Int
+t3 = Node3 $ \i -> Node3 $ \j -> Leaf3 (i + j)
+
+-- fmap id x = x    -->  fmap mindig megőrzi a konstruktorokat
 
 instance Functor (Tree3 i) where
   fmap :: (a -> b) -> Tree3 i a -> Tree3 i b
   fmap f (Leaf3 a) = Leaf3 (f a)
   fmap f (Node3 g) = Node3 (\i -> fmap f (g i))
-      -- i-edik map-elt fa: i-edik eredeti fa map-elése
+     -- "i-edik map-elt részfa = i-edik eredeti részfa map-elése"
+     --
 
 instance Functor (Either a) where
   fmap = undefined
