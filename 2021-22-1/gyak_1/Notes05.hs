2973999addd4a9552da4c03e3acbe574a5912444
AndrasKovacs <puttamalac@gmail.com>
2021-10-10 11:24:15 +0200

    notes 05

--- 2021-22-1/gyak_1/Notes05.hs
+++ 2021-22-1/gyak_1/Notes05.hs
@@ -1,6 +1,36 @@
 {-# LANGUAGE DeriveFunctor #-}
 
-import Control.Monad
+import Control.Monad  -- replicateM_
+
+-- következő beadandó:
+--    Maybe/Either monád, rekurzív függvény (lista/fa)
+
+-- beadandó
+------------------------------------------------------------
+
+f :: a -> (a -> Maybe b) -> (b -> Maybe c) -> (c -> Maybe d) -> Maybe d
+f a g1 g2 g3 = do
+  b <- g1 a
+  c <- g2 b
+  d <- g3 c
+  return d
+
+  -- b <- g1 a     -- Monad törvény
+  -- c <- g2 b     -- (return bind-nak jobb identitása)
+  -- g3 c
+
+f' :: a -> (a -> Maybe b) -> (b -> Maybe c) -> (c -> Maybe d) -> Maybe d
+f' a g1 g2 g3 = case g1 a of
+  Nothing -> Nothing
+  Just b -> case g2 b of
+    Nothing -> Nothing
+    Just c -> g3 c
+
+-- (>=>)
+f'' :: a -> (a -> Maybe b) -> (b -> Maybe c) -> (c -> Maybe d) -> Maybe d
+f'' a g1 g2 g3 = (g1 >=> g2 >=> g3) a
+               -- (g3 . g2 . g1) a
+
 
 -- IO monád
 --------------------------------------------------------------------------------
@@ -15,14 +45,24 @@ import Control.Monad
 -- Írj egy függvényt, ami beolvas egy sort, majd a sort kinyomtatja annyiszor,
 -- ahány karakter van a sorban!
 io2 :: IO ()
-io2 = undefined
+io2 = do
+  let action = getLine    -- let         : mellékhatás nélküli definíció
+  l <- action             -- x <- action : action végrehajtása
+  let n = length l  -- do belsejében nem kell "in" "let" után
+  replicateM_ n (putStrLn l)
 
 -- Írj egy függvényt, ami addig olvas be ismételten sorokat, amíg a sor nem
 -- tartalmaz 'x' karaktert. Ha a sorban 'x' van, akkor a program nyomtassa ki az
 -- összes eddig beolvasott sort és térjen vissza.
 io3 :: IO ()
-io3 = undefined
-
+io3 = loop [] where
+  loop :: [String] -> IO ()
+  loop lines = do
+    l <- getLine
+    if elem 'x' l then do
+      print lines
+    else do
+      loop (l:lines)
 
 -- Olvass be két sort, nyomtasd ki közülük a rövidebbet.
 io4 :: IO ()
@@ -34,14 +74,44 @@ io4 = undefined
 -- Definiáld a következő függvényeket tetszőlegesen, de típushelyesen.
 -- return, (>>=), fmap
 -- (alternatív: do-notáció)
+-- (csak return, >>=, do használatával definiáljuk)
+
+-- fmap újradefiniálása
 f1 :: Monad m => (a -> b) -> m a -> m b
-f1 = undefined
+f1 f ma = do
+  a <- ma
+  return (f a)
 
 f2 :: Monad m => m a -> m b -> m (a, b)
-f2 = undefined
+f2 ma mb = do
+  a <- ma
+  b <- mb
+  return (a, b)
 
+-- standard függvény: join
 f3 :: Monad m => m (m a) -> m a
-f3 = undefined
+f3 mma = do
+  ma <- mma
+  a  <- ma
+  return a
+
+-- f3 mma = do
+--   ma <- mma
+--   ma
+
+-- konkrét join példa:
+f3' :: IO (IO ())
+f3' = do
+  b <- fmap (read :: String -> Bool) getLine
+  if b then return (putStrLn "hello")
+       else return (putStrLn "world")
+
+-- futtatja f3'-at, utána annak az eredményét is futattja
+f3'' :: IO ()
+f3'' = join f3'
+
+-- opcionális házi:
+--   definiáljuk bind-ot join-ból, és fordítva
 
 f4 :: Monad m => m (a -> b) -> m a -> m b
 f4 = undefined
@@ -61,6 +131,25 @@ f8 = undefined
 
 -- State monád definíció
 --------------------------------------------------------------------------------
+
+-- nézzük EA + jegyzet
+
+-- "State s a" konkrét használat
+--   State s a  :  olyan művelet, ami 1 db "s" típusú
+--                 mutábilis változót tud írni olvasni,
+--                 visszatérési érték típusa "a"
+
+-- "State s :: * -> *" ez a monád
+
+-- put    :: s -> State s ()        : "s" értékkel írja az állapotot
+-- get    :: State s s              : kiolvassa az állapotot
+-- modify :: (s -> s) -> State s () : egy függvényt alkalmaz az állapoton
+
+--              művelet    kezdő állapot     (érték, végső állapot)
+-- runState  :: State s a ->      s        -> (a, s)
+
+-- evalState :: State s a -> s -> a
+-- execState :: State s a -> s -> s
 
 
 newtype State s a = State {runState :: s -> (a, s)} deriving Functor
@@ -88,25 +177,46 @@ evalState ma = fst . runState ma
 execState :: State s a -> s -> s
 execState ma = snd . runState ma
 
-
--- definiálj egy függvényt, ami a lista állapotot kiegészít egy elemmel
+-- definiálj egy függvényt, ami a lista állapotot kiegészíti egy elemmel
+--
 push :: a -> State [a] ()
-push a = undefined
+push a = modify (\as -> a:as)
+      -- modify (a:)
 
 -- runState (push 10) [] == ((), [10])
+-- runState (push 10 >> push 10 >> push 20) [] == ((), [20, 10, 10])
+-- runState (replicateM_ 10 (push 0)) []
 
--- ha az állapot lista nem üres, akkor a következő függvény leveszi az első elemet
--- és visszaadja Just értékként, egyébként Nothing-ot ad.
+-- push' :: a -> [a] -> [a]
+-- push' = (:)
+
+-- lényeg: sok algoritmus van, ahol a mutáció
+-- egyszerűsíti és áttekinthetővé teszi az implementációt
+
+-- ha az állapot lista nem üres, akkor a következő függvény leveszi az első
+-- elemet és visszaadja Just értékként, egyébként Nothing-ot ad.
 pop :: State [a] (Maybe a)
-pop = undefined
+pop = do
+  as <- get        -- get :: State [a] [a]
+  case as of
+    []   -> return Nothing
+    a:as -> do
+      put as  -- felülírja az állapotot as-el
+      return (Just a)
 
 -- runState pop [0, 1, 2] == (Just 0, [1, 2])
 -- runState pop []        == (Nothing, [])
 
-
--- Írj egy függvényt, ami kizárólag push, pop és rekurzió felhasználásával map-eli az állapot listát.
+-- Írj egy függvényt, ami kizárólag push, pop és rekurzió felhasználásával
+-- map-eli az állapot listát.
 mapPP :: (a -> a) -> State [a] ()
-mapPP = undefined
+mapPP f = do
+  ma <- pop
+  case ma of
+    Nothing -> return ()
+    Just a  -> do
+      mapPP f
+      push (f a)
 
 -- execState (mapPP (+10)) [0, 1, 2] == [10, 11, 12]
 
@@ -117,23 +227,39 @@ mapPP = undefined
 maxs :: [Int] -> [Int]
 maxs = undefined
 
-
 data Tree a = Leaf a | Node (Tree a) (Tree a)
   deriving (Functor, Show)
 
--- Definiálj egy függvényt, ami kicsérli egy fa leveleiben tárolt értékeket
--- balról jobbra haladva egy megadott lista elemeire.
--- Használj State monádot!
+-- Definiálj egy függvényt, ami kicseréli egy fa leveleiben tárolt értékeket
+-- balról jobbra haladva egy megadott lista elemeire. Használj State monádot!
 
--- pl: replaceLeaves [10, 20, 30] (Node (Leaf 2) (Leaf 3)) == Node (Leaf 10) (Leaf 20)
+-- pl: replaceLeaves [10, 20, 30] (   Node (Leaf 2) (Leaf 3))
+--                                 == Node (Leaf 10) (Leaf 20)
 --     replacereplaceLeaves [5] (Leaf 10) == Leaf 5
---     replacereplaceLeaves [5] (Node (Leaf 0) (Node (Leaf 0) (Leaf 0))) ==
+--     replacereplaceLeaves [5]
+--        (Node (Leaf 0) (Node (Leaf 0) (Leaf 0))) ==
 --        (Node (Leaf 5) (Node (Leaf 0) (Leaf 0)))
 
-
+-- evalState :: State s a -> s -> a
 replaceLeaves :: [a] -> Tree a -> Tree a
-replaceLeaves = undefined
+replaceLeaves as t = evalState (go t) as where
 
+  go :: Tree a -> State [a] (Tree a)
+  go (Leaf a)   = do
+    ma' <- pop
+    case ma' of
+      Nothing -> return (Leaf a)
+      Just a' -> return (Leaf a')
+  go (Node l r) = do
+    l' <- go l
+    r' <- go r
+    return (Node l' r')
+
+-- opcionális házi: replaceLeaves State *nélkül*
+--   ha elfogyik a lista, akkor ne járjuk be tovább a fát
+--     ( State + exception mellékhatás kombinálása )
+--     N darab különböző monád : lehet-e kombinálni?
+--       (lásd: "monád transzformer")
 
 -- Definiáld a függvényt, ami megfordítja a fa leveleiben tárolt értékek
 -- sorrendjét!  tipp: használhatod a replaceLeaves függvényt.
