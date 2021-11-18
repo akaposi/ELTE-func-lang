bd24cc92a7422a6d46b8e655e440999fc1adfdcb
Rafaël Bocquet <rbocquet@localhost.localdomain>
2021-11-18 18:12:33 +0100

    Add gyak_3/Tut10.hs

---
+++ 2021-22-1/gyak_3/Tut10.hs
@@ -0,0 +1,216 @@
+{-# OPTIONS -Wincomplete-patterns #-}
+{-# LANGUAGE InstanceSigs, DeriveFunctor, DeriveFoldable, DeriveTraversable #-}
+
+--------------------------------------------------------------------------------
+
+import Data.Foldable
+import Data.Traversable
+import Control.Monad
+import Control.Applicative
+import Data.Char 
+
+import Debug.Trace
+
+newtype Parser a = Parser {runParser :: String -> Maybe (a, String)}
+  deriving(Functor)
+
+instance Applicative Parser where
+  pure  = return
+  (<*>) = ap
+
+instance Monad Parser where
+  return :: a -> Parser a
+  return a = Parser $ \s -> Just (a, s)
+
+  (>>=) :: Parser a -> (a -> Parser b) -> Parser b
+  Parser f >>= g = Parser $ \s -> case f s of
+    Nothing      -> Nothing
+    Just (a, s') -> runParser (g a) s'
+
+instance Alternative Parser where
+  -- The empty parser always fails.
+  empty :: Parser a
+  empty = Parser $ \_ -> Nothing
+
+  -- The parser (p1 <|> p2) first tries to run the parser p1. 
+  --   If p1 fails, the parser p2 is used instead.
+  (<|>) :: Parser a -> Parser a -> Parser a
+  Parser f <|> Parser g = Parser $ \s -> case f s of
+    Nothing -> g s
+    x       -> x
+
+eof :: Parser ()
+eof = Parser $ \s -> case s of
+  [] -> Just ((), [])
+  _  -> Nothing
+
+-- The parser `satisfy p` accepts any character that satisfies the predicate p.
+satisfy :: (Char -> Bool) -> Parser Char
+satisfy f = Parser $ \s -> case s of
+  c:s | f c -> Just (c, s)
+  _         -> Nothing
+
+debug :: String -> Parser a -> Parser a
+debug msg pa = Parser $ \s -> trace (msg ++ " : " ++ s) (runParser pa s)
+
+-- The parser `char c` recognizes exactly the character c.
+char :: Char -> Parser ()
+char c = () <$ satisfy (==c)
+
+alpha :: Parser Char
+alpha = satisfy (\c -> 'a' <= c && c <= 'z')
+
+-- The parser anyChar accepts any character.
+anyChar :: Parser Char
+anyChar = satisfy (\_ -> True)
+
+-- The parser `string s` recognizes exactly the string s.
+string :: String -> Parser ()
+string = traverse_ char
+
+-- In Control.Applicative:
+
+-- many :: Parser a -> Parser [a]
+--   `many p` tries to run the parser p as many times as possible.
+
+-- some :: Parser a -> Parser [a]
+--   `some p` tries to run the parser p as many times as possible.
+--   The parser p should suceed at least once.
+
+many' :: Parser a -> Parser [a]
+many' p = some' p <|> pure []
+
+some' :: Parser a -> Parser [a]
+some' p = do x <- p; xs <- many' p; pure (x:xs)
+
+-- `sepBy pa psep` runs the parser pa as many times as possible, 
+--   separated by the parser psep.
+sepBy :: Parser a -> Parser sep -> Parser [a]
+sepBy pa psep = sepBy1 pa psep <|> pure []
+
+-- `sepBy pa psep` runs the parser pa as many times as possible, 
+--   separated by the parser psep.
+--   The parser pa should suceed at least once.
+sepBy1 :: Parser a -> Parser sep -> Parser [a]
+sepBy1 pa psep = (:) <$> pa <*> many (psep *> pa)
+
+-------------------------------------------------------------------------------
+
+digit :: Parser Int
+digit = digitToInt <$> satisfy isDigit
+
+posInt :: Parser Int
+posInt = fmap (foldl' (\x d -> 10*x+d) 0) (some digit)
+
+negInt :: Parser Int
+negInt = char '-' *> (negate <$> posInt)
+
+int :: Parser Int
+int = posInt <|> negInt
+
+--------------------------------------------------------------------------------
+
+ws :: Parser [Char]
+ws = many (satisfy isSpace)
+
+char' :: Char -> Parser ()
+char' c = char c <* ws
+
+--   char' '+'
+-- "+    10"
+
+string' :: String -> Parser ()
+string' s = string s <* ws
+
+keywords :: [String]
+keywords = [ "if", "then", "else", "while", "do", "end"
+           , "let", "in"
+           ]
+
+pKeyword :: String -> Parser ()
+pKeyword str = do
+  string str
+  (do satisfy isLetter; empty) <|> pure ()
+
+pIdent :: Parser String
+pIdent = do
+  x <- some (satisfy isLetter) <* ws
+  if x `elem` keywords then empty
+                       else pure x
+
+--------------------------------------------------------------------------------
+-- Parser for a small imperative language
+
+data Exp
+  = IntLit Int    -- integer literals
+  | BoolLit Bool  -- boolean literals
+  | Add Exp Exp   -- e1 + e2, left associative
+  | Mul Exp Exp   -- e1 * e2, left associative
+  | Eq  Exp Exp   -- e1 == e2, not associative
+  | Var String    -- variables
+  deriving (Eq, Show)
+
+type Program = [Statement]
+
+data Statement
+  = Assign String Exp               -- x := exp
+  | IfThenElse Exp Program Program  -- if e1 then p1 else p2 end
+  | While Exp Program               -- while e do p end
+  deriving (Eq, Show)
+
+
+-- precedence levels for expressions:
+--  variables, literals, parentheses
+--  *, left associative
+--  +, left associative
+--  ==, not associative
+
+pAtom :: Parser Exp
+pAtom = undefined
+
+pMul :: Parser Exp
+pMul = undefined
+
+pAdd :: Parser Exp
+pAdd = undefined
+
+pEq :: Parser Exp
+pEq = undefined
+
+pExp :: Parser Exp
+pExp = pEq
+
+pStatement :: Parser Statement 
+pStatement = undefined
+
+pProgram :: Parser Program
+pProgram = sepBy pStatement (char' ';')
+
+pTop :: Parser Program
+pTop = ws *> pProgram <* eof
+
+--------------------------------------------------------------------------------
+
+-- Parser for a small Haskell-like functional language
+
+data Exp2
+  = Var2 String           -- variables
+  | Lambda2 String Exp2   -- \x . e
+  | App2                  -- f t
+  | Let2 String Exp2 Exp2 -- let x = u in t
+  deriving (Eq, Show)
+
+pAtom2 :: Parser Exp2
+pAtom2 = undefined
+
+pApp2 :: Parser Exp2
+pApp2 = undefined
+
+pLam2 :: Parser Exp2
+pLam2 = undefined
+
+pLet2 :: Parser Exp2
+pLet2 = undefined
+
+pExp2 :: Parser Exp2
+pExp2 = undefined
