93d88e4de91f8841b8621ded61caff33ed202910
AndrasKovacs <puttamalac@gmail.com>
2022-04-01 16:27:45 +0200

    ea 08

---
+++ 2021-22-2/ea/Notes08.hs
@@ -0,0 +1,290 @@
+{-# language DeriveFunctor, InstanceSigs #-}
+
+-- Operátorok folyt, prec kombinátor, azonosítók, kulcsszavak, kiértékelés
+--------------------------------------------------------------------------------
+
+import Control.Applicative
+import Control.Monad
+import Debug.Trace
+import Data.Char    -- isSpace, isDigit
+
+
+-- Parser lib
+--------------------------------------------------------------------------------
+
+newtype Parser a = Parser {runParser :: String -> Maybe (a, String)}
+  deriving (Functor)
+
+instance Applicative Parser where
+  pure  = return
+  (<*>) = ap
+
+instance Monad Parser where
+  return :: a -> Parser a
+  return a = Parser $ \s -> Just (a, s)   -- nincs hatás
+
+  (>>=) :: Parser a -> (a -> Parser b) -> Parser b
+  Parser f >>= g = Parser $ \s -> do {(a, s) <- f s; runParser (g a) s}
+
+-- pontosan az üres inputot olvassuk
+eof :: Parser ()
+eof = Parser $ \s -> case s of
+  [] -> Just ((), [])
+  _  -> Nothing
+
+-- olvassunk egy karaktert az input elejéről, amire igaz egy feltétel
+satisfy :: (Char -> Bool) -> Parser Char
+satisfy f = Parser $ \s -> case s of
+  c:s | f c -> Just (c, s)
+  _         -> Nothing
+
+-- olvassunk egy konkrét karaktert
+char :: Char -> Parser ()
+char c = () <$ satisfy (==c)
+
+-- olvassunk egy konkrét String-et
+string :: String -> Parser ()   -- String ~ [Char]
+string s = mapM_ char s         -- egymás után olvasom az összes Char-t a String-ben
+
+
+instance Alternative Parser where
+  -- mindig hibázó parser
+  empty :: Parser a
+  empty = Parser $ \_ -> Nothing
+
+  -- választás két parser között
+  Parser f <|> Parser g = Parser $ \s -> case f s of
+    Nothing -> g s
+    res     -> res
+
+-- Control.Applicative-ból:
+--    many  :: Parser a -> Parser [a]       -- 0-szor vagy többször futtatja
+--    some  :: Parser a -> Parser [a]       -- 1-szer vagy többször futtatja
+
+many_ :: Parser a -> Parser ()
+many_ pa = () <$ many pa
+
+some_ :: Parser a -> Parser ()
+some_ pa = () <$ some pa
+
+-- Control.Applicative-ból:
+-- optional :: Parser a -> Parser (Maybe a)   -- hibát értékként visszaadja (soha nem hibázik)
+-- optional pa = (Just <$> pa) <|> pure Nothing
+
+-- 0 vagy 1 eredményt olvasunk
+optional_ :: Parser a -> Parser ()
+optional_ pa = () <$ optional pa
+
+-- olvassunk 1 vagy több pa-t, psep-el elválasztva
+sepBy1 :: Parser a -> Parser sep -> Parser [a]
+sepBy1 pa psep = do
+  a  <- pa
+  as <- many (psep *> pa)
+  pure (a:as)
+
+-- olvassunk 0 vagy több pa-t, psep-el elválasztva
+sepBy :: Parser a -> Parser sep -> Parser [a]
+sepBy pa psep = sepBy1 pa psep <|> pure []
+
+debug :: String -> Parser a -> Parser a
+debug msg pa = Parser $ \s -> trace (msg ++ " : " ++ s) (runParser pa s)
+
+
+-- token/ws parsing
+
+ws :: Parser ()
+ws = many_ (satisfy isSpace)
+
+satisfy' :: (Char -> Bool) -> Parser Char
+satisfy' f = satisfy f <* ws
+
+char' :: Char -> Parser ()
+char' c = char c <* ws
+
+string' :: String -> Parser ()
+string' s = string s <* ws
+
+topLevel :: Parser a -> Parser a
+topLevel pa = ws *> pa <* eof
+
+-- operátor segédfüggvények
+
+rightAssoc :: (a -> a -> a) -> Parser a -> Parser sep -> Parser a
+rightAssoc f pa psep = foldr1 f <$> sepBy1 pa psep
+
+leftAssoc :: (a -> a -> a) -> Parser a -> Parser sep -> Parser a
+leftAssoc f pa psep = foldl1 f <$> sepBy1 pa psep
+
+nonAssoc :: (a -> a -> a) -> Parser a -> Parser sep -> Parser a
+nonAssoc f pa psep = do
+  exps <- sepBy1 pa psep
+  case exps of
+    [e]      -> pure e
+    [e1,e2]  -> pure (f e1 e2)
+    _        -> empty
+
+
+--------------------------------------------------------------------------------
+
+data Exp =
+    IntLit Int          -- int literál (pozitív)
+  | Add Exp Exp         -- e + e
+  | Mul Exp Exp         -- e * e
+  | BoolLit Bool        -- true|false
+  | And Exp Exp         -- e && e
+  | Or Exp Exp          -- e || e
+  | Not Exp             -- not e
+  | Eq Exp Exp          -- e == e
+  | Var String          -- (változónév)
+  deriving (Eq, Show)
+
+{-
+Változónév: nemüres alfabetikus string
+
+Kötési erősségek csökken sorrendben:
+  - atom: zárójelezett kifejezés, literál, változónév
+  - not alkalmazás
+  - *  : jobbra asszoc
+  - +  : jobbra asszoc
+  - && : jobbra asszoc
+  - || : jobbra asszoc
+  - == : nem asszoc
+-}
+
+posInt' :: Parser Int
+posInt' = do
+  digits <- some (satisfy isDigit)
+  ws
+  pure (read digits)
+
+-- változónév/kulcsszó olvasás
+
+-- kulcsszavak:
+keywords :: [String]
+keywords = ["not", "true", "false", "while", "if", "do", "end", "then", "else"]
+
+ident' :: Parser String
+ident' = do
+  x <- some (satisfy isAlpha) <* ws
+  if elem x keywords
+    then empty
+    else pure x
+
+keyword' :: String -> Parser ()
+keyword' str = do
+  x <- some (satisfy isAlpha) <* ws
+  if x == str
+    then pure ()
+    else empty
+
+atom :: Parser Exp
+atom =
+        (Var <$> ident')
+    <|> (IntLit <$> posInt')
+    <|> (BoolLit True <$ keyword' "true")
+    <|> (BoolLit False <$ keyword' "false")
+    <|> (char' '(' *> eqExp <* char' ')')
+
+pNot :: Parser Exp
+pNot =  (keyword' "not" *> (Not <$> atom))
+    <|> atom
+
+mulExp :: Parser Exp
+mulExp = rightAssoc Mul pNot (char' '*')
+
+addExp :: Parser Exp
+addExp = rightAssoc Add mulExp (char' '+')
+
+andExp :: Parser Exp
+andExp = rightAssoc And addExp (string' "&&")
+
+orExp :: Parser Exp
+orExp = rightAssoc Or andExp (string' "||")
+
+eqExp :: Parser Exp
+eqExp = nonAssoc Eq orExp (string' "==")
+
+pExp :: Parser Exp
+pExp = topLevel eqExp
+
+-- összes parser konvenció (kódstrukturálás), amit megnézünk:
+--   - token/ws/topLevel konvenció
+--   - operátor parsolás konvenció, segédfüggvények
+--   - azonosítók és kulcsszavak kezelése
+
+-- extra feature-ök, amit nem néztünk (opcionális házi)
+--   - több operátor ugyanazon az erősségi szinten
+--   - operátor konvenciót még magasabb szinten refaktorálni
+--     (lásd Parsec, Megaparsec library-k, expression parser)
+--     parserOperators :: OperatorTable a -> Parser a
+--   - informatív hibaüzenet (nem egyszerű implementálni)
+
+
+-- Exp interpreter
+
+data Val = VInt Int | VBool Bool
+  deriving (Eq, Show)
+
+type Env = [(String, Val)]
+
+evalExp :: Env -> Exp -> Val
+evalExp env e = case e of
+  IntLit n  -> VInt n
+  BoolLit b -> VBool b
+  Add e1 e2 -> case (evalExp env e1, evalExp env e2) of
+    (VInt n1, VInt n2) -> VInt (n1 + n2)
+    _                  -> error "type error"
+  Mul e1 e2 -> case (evalExp env e1, evalExp env e2) of
+    (VInt n1, VInt n2) -> VInt (n1 * n2)
+    _                  -> error "type error"
+  Or e1 e2 -> case (evalExp env e1, evalExp env e2) of
+    (VBool b1, VBool b2) -> VBool (b1 || b2)
+    _                    -> error "type error"
+  And e1 e2 -> case (evalExp env e1, evalExp env e2) of
+    (VBool b1, VBool b2) -> VBool (b1 && b2)
+    _                    -> error "type error"
+  Eq e1 e2 -> case (evalExp env e1, evalExp env e2) of
+    (VBool b1, VBool b2) -> VBool (b1 == b2)
+    (VInt n1,  VInt n2 ) -> VBool (n1 == n2)
+    _                    -> error "type error"
+  Not e -> case evalExp env e of
+    VBool b -> VBool (not b)
+    _       -> error "type error"
+  Var x -> case lookup x env of
+    Just v  -> v
+    Nothing -> error $ "name not in scope: " ++ x
+
+run :: Env -> String -> Val
+run env str = case runParser pExp str of
+  Nothing     -> error "parse error"
+  Just (e, _) -> evalExp env e
+
+-- extra szépítés: eval függvényben kódismétlést csökkenteni
+--   fordítási idejű kiértékelés
+--     ("staged compilation")
+--     ("metaprogramming")
+--     (ezek segítségével lehet absztrakciót + hatékonyságot kombinálni)
+--     (MetaOCaml)
+--     (Scala: LMS framework)
+--       (C++ template)
+--       (Haskell: Template Haskell)
+
+
+-- State + eval (minimális imperatív nyelv)
+------------------------------------------------------------
+
+type Program = [Statement]  -- st1; st2; st3; ... st4
+
+data Statement
+  = Assign String Exp       -- x := e
+  | While Exp Program       -- while e do prog end
+  | If Exp Program Program  -- if e then prog1 else prog2 end
+  deriving (Eq, Show)
+
+-- statement :: Parser Exp
+-- statement =
+--         (Assign <$> ident' <*> (string' ":=" *> eqExp))
+--     <|> (While <$> _ <*> _)
+--     <|> (If <$> _ <*> _ <*> _)
+
+-- ("környezetfüggetlen" szintaxist mindig lehet Applicative-an definiálni)
