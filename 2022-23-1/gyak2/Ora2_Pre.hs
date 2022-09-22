9d87ee5a0cd746196b6b16d62adafdad9552a03a
Thrithralas <48440160+Thrithralas@users.noreply.github.com>
2022-09-22 15:48:23 +0200

    Óra2 Pre

---
+++ 2022-23-1/gyak2/Ora2_Pre.hs
@@ -0,0 +1,89 @@
+{-# OPTIONS_GHC -Wincomplete-patterns #-}
+
+module Ora2 where
+
+f13 :: (a -> a -> b) -> ((a -> b) -> a) -> b
+f13 = undefined
+
+-- Ord instance listára
+data List a = Nil | Cons a (List a)
+infixr 5 `Cons`
+
+
+-- Szabályok:
+-- Ha az egyik fejelem nagyobb, akkor az a lista nagyobb
+-- Ha egyenlőek a lista maradéka dönti el
+-- Ha az egy lista üres a másik nagyobb
+instance Ord a => Ord (List a) where
+    (<=) = undefined
+
+
+-- Milyen rekurzív adattípusok vannak még?
+-- Pl a bináris fa egy ilyen (Algo 1)
+
+-- Minden levelében van egy érték és minden elágazásban van egy érték
+data Tree a
+-- Csak az elágazásokban van érték
+data BranchOnlyTree a
+-- Csak a levelekben van érték
+data LeafOnlyTree a
+
+instance Eq a => Tree a where
+    (==) = undefined
+
+instance Eq a => BranchOnlyTree a where
+    (==) = undefined
+
+instance Eq a => LeafOnlyTree a where
+    (==) = undefined
+
+-- ezen a tárgyon inkább csak a LeafOnlyTree-t szeretjük használni
+
+treeHeight :: Num a => Tree b -> a
+treeHeight = undefined
+
+
+-- Listaszerű függvények
+
+treeSum :: Num a => Tree a -> a
+treeSum = undefined
+
+boTreeSum :: Num a => BranchOnlyTree a -> a
+boTreeSum = undefined
+
+loTreeSum :: Num a => LeafOnlyTree a -> a
+loTreeSum = undefined
+
+treeProduct :: Num a => Tree a -> a
+treeProduct = undefined
+
+elemTree :: Eq a => a -> Tree a -> Bool
+elemTree = undefined
+
+-- hasonlóak ezek a lista fv-ekhez: sum, product és elem
+-- lehetne-e az olyan típusokat amelyekre vannak ilyen műveletek generalizálni!
+
+-- Foldable típusosztály: olyan típusokat ír le amelyeket végig lehet hajtogatni
+
+-- Itt elhagytuk az a paramétert, ugyanis ennek minden a típusra működnie kell!
+-- GHCi-ben megnézhetjük :i Foldable kiírja, hogya egy (Type -> Type) típust vár
+
+-- Először írjuk meg listára
+
+instance Foldable List where
+    foldr = undefined
+
+instance Foldable Tree where
+    foldr = undefined
+
+-- Foldable-el kapunk menő műveleteket pl sum, minimum, maximum stb
+
+-- Mi lehet még Foldable? Igazából akármi amibe vannak 'a' típusú értékek és nincs 'a' paraméterű függvény
+data Maybe' a = Just a | Nothing
+data NonEmpty' a = a :| [a]
+
+instance Foldable Maybe' where
+    foldr = undefined
+
+instance Foldable NonEmpty'' where
+    foldr = undefined
