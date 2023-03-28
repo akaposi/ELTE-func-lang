6e648e056f5e56a3f7037e2497dee2df2323cddf
Daerile <38301066+Daerile@users.noreply.github.com>
2023-03-28 17:16:52 +0200

    Add files via upload

---
+++ 2022-23-2/gyak3/Gy05_pre.hs
@@ -0,0 +1,148 @@
+{-# language InstanceSigs, DeriveFunctor #-}
+{-# options_ghc -Wincomplete-patterns #-}
+
+import Control.Monad
+
+-- IO monád
+--------------------------------------------------------------------------------
+
+-- getLine  :: IO String             -- beolvas String-et
+-- readLn   :: IO _                  -- beolvas bármit
+-- print    :: Show a => a -> IO ()  -- kinyomtat értéket
+-- putStrLn :: String -> IO ()       -- String-et nyomtat ki
+
+-- (>>=)  :: IO a -> (a -> IO b) -> IO b
+-- return :: a -> IO a
+-- fmap   :: (a -> b) -> IO a -> IO b
+
+
+-- Írj egy függvényt, ami beolvas egy sort, majd visszaadja a sorban található
+-- 'a' betűk számát.
+io1 :: IO ()
+io1 = undefined
+
+
+-- Írj egy függvényt, ami beolvas egy sort, majd a sort kinyomtatja annyiszor,
+-- ahány karakter van a sorban!
+io2 :: IO ()
+io2 = undefined
+
+-- Írj egy függvényt, ami addig olvas be ismételten sorokat, amíg a sor nem
+-- tartalmaz 'x' karaktert.
+io3 :: IO ()
+io3 = undefined
+
+-- Írj egy függvényt, ami addig olvas be ismételten sorokat, amíg a sor nem
+-- tartalmaz 'x' karaktert. Ha a sorban 'x' van, akkor a program nyomtassa ki az
+-- összes eddig beolvasott sort és térjen vissza.
+io3' :: IO ()
+io3' = undefined
+
+
+-- A következőt ismételd végtelenül: olvass be egy sort, majd nyomtasd ki a
+-- sorban a kisbetűk számát.  A Ctrl-c-c -vel lehet megszakítani a futtatást
+-- ghci-ben.
+io4 :: IO ()
+io4 = undefined
+
+--------------------------------------------------------------------------------
+
+-- Definiáld a következő függvényeket tetszőlegesen, de típushelyesen.
+
+f1 :: Monad m => (a -> b) -> m a -> m b
+f1 = undefined
+
+f2 :: Monad m => m a -> m b -> m (a, b)
+f2 = undefined
+
+f3 :: Monad m => m (m a) -> m a
+f3 = undefined
+
+f4 :: Monad m => m (a -> b) -> m a -> m b
+f4 = undefined
+
+f5 :: Monad m => (a -> m b) -> m a -> m b
+f5 = undefined
+
+f6 :: Monad m => (a -> b -> c) -> m a -> m b -> m c
+f6 = undefined
+
+f7 :: Monad m => (a -> b -> c -> d) -> m a -> m b -> m c -> m d
+f7 = undefined
+
+f8 :: Monad m => (a -> m b) -> (b -> m c) -> a -> m c
+f8 = undefined
+
+-- State monád definíció
+--------------------------------------------------------------------------------
+
+newtype State s a = State {runState :: s -> (a, s)} deriving Functor
+
+instance Applicative (State s) where
+  pure  = return
+  (<*>) = ap
+
+instance Monad (State s) where
+  return a = State (\s -> (a, s))
+  State f >>= g = State (\s -> case f s of (a, s') -> runState (g a) s')
+
+get :: State s s
+get = State (\s -> (s, s))
+
+put :: s -> State s ()
+put s = State (\_ -> ((), s))
+
+modify :: (s -> s) -> State s ()
+modify f = do {s <- get; put (f s)}
+
+evalState :: State s a -> s -> a
+evalState ma = fst . runState ma
+
+execState :: State s a -> s -> s
+execState ma = snd . runState ma
+
+
+
+-- Definiálj egy függvényt, ami a lista állapotot kiegészíti egy elemmel
+push :: a -> State [a] ()
+push = undefined
+
+-- példák:
+-- runState (push 10) [] == ((), [10])
+-- runState (push 10 >> push 10 >> push 20) [] == ((), [20, 10, 10])
+
+
+-- Ha az állapot lista nem üres, akkor a következő függvény leveszi az első
+-- elemet és visszaadja Just értékként, egyébként Nothing-ot ad.
+pop :: State [a] (Maybe a)
+pop = undefined
+
+
+-- Írj egy függvényt, ami egy Int listában minden elemet kicserél az addigi
+-- elemek maximumára.  Tegyük fel, hogy a lista nem-negatív számokat tartalmaz.
+-- Használd a (State Int)-et. Legyen az állapot a jelenlegi maximális Int.
+maxs :: [Int] -> [Int]
+maxs = undefined
+
+
+-- Definiálj egy függvényt, ami kicseréli egy fa leveleiben tárolt értékeket
+-- balról jobbra haladva egy megadott lista elemeire. Használj State monádot!
+
+data Tree a = Leaf a | Node (Tree a) (Tree a)
+  deriving (Functor, Show)
+
+-- pl: replaceLeaves [10, 20, 30] (   Node (Leaf 2) (Leaf 3))
+--                                 == Node (Leaf 10) (Leaf 20)
+--     replacereplaceLeaves [5] (Leaf 10) == Leaf 5
+--     replacereplaceLeaves [5]
+--        (Node (Leaf 0) (Node (Leaf 0) (Leaf 0))) ==
+--        (Node (Leaf 5) (Node (Leaf 0) (Leaf 0)))
+
+replaceLeaves :: [a] -> Tree a -> Tree a
+replaceLeaves = undefined
+
+
+-- Definiáld a függvényt, ami megfordítja a fa leveleiben tárolt értékek
+-- sorrendjét!  tipp: használhatod a replaceLeaves függvényt.
+reverseElems :: Tree a -> Tree a
+reverseElems = undefined
