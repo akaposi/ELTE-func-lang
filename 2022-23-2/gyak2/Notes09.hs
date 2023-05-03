2796631f49919217f8dae8e3782c13e1cc07aa41
András Kovács <puttamalac@gmail.com>
2023-05-03 20:13:06 +0200

    gyak2 09

--- 2022-23-2/gyak2/Notes09.hs
+++ 2022-23-2/gyak2/Notes09.hs
@@ -5,6 +5,12 @@ import Control.Monad
 import Control.Applicative
 import Data.Char
 
+-- Megoldás
+--------------------------------------------------------------------------------
+
+f :: Parser ()
+f = string "kutya" <|> string "macska" <|> string "béka"
+
 --------------------------------------------------------------------------------
 
 newtype Parser a = Parser {runParser :: String -> Maybe (a, String)}
@@ -78,19 +84,6 @@ some_ :: Parser a -> Parser ()
 some_ pa = () <$ some pa
 
 
-------------------------------------------------------------
--- regex-ek: típus Parser ()  (arra vagyunk kíváncsiak, hogy illeszkedik-e
---                             egy regex az inputra)
---
---  Parser ()            regex
---------------------------------
---   char c               c
---  p1 >> p2             p1p2
---  p1 <|> p2            p1|p2
---   many_ p              p*
---   some_ p              p+
---    eof                 $
-
 ----------------------------------------
 
 -- Definiáljuk a regex-ekből ismerős ? operátort
@@ -118,6 +111,54 @@ pPos = do
   ds <- some pDigit
   pure $ sum $ zipWith (*) (reverse ds) (iterate (*10) 1)
 
+-- Ismétlés:
+--------------------------------------------------------------------------------
+
+-- runParser :: Parser a -> String -> Maybe (a, String)
+
+
+-- char :: Char -> Parser ()                 -- konkrét karakter olvasása
+-- string :: String -> Parser ()             -- konkrét String olvasása
+-- eof :: Parser ()                          -- üres input olvasása
+-- (<|>) :: Parser a -> Parser a -> Parser a -- választás ("vagy")
+-- many :: Parser a -> Parser [a]            -- 0-szor vagy többször olvasás
+-- some :: Parser a -> Parser [a]            -- 1-szer vagy többször olvasás
+-- many_ :: Parser a -> Parser ()
+-- some_ :: Parser a -> Parser ()
+-- satisfy :: (Char -> Bool) -> Parser Char  -- 1 karakter olvasása amire
+                                             -- igaz a feltétel
+
+-- Functor instance
+--   fmap :: (a -> b) -> Parser a -> Parser b
+
+-- Monad instance
+--   return :: a -> Parser a
+--   (>>=) :: Parser a -> (a -> Parser b) -> Parser b
+
+-- írj egy Parser-t, ami 0 vagy több 'A' karaktert olvas, és
+-- visszaadja, hogy hányat olvasott (mint Int)
+countAs :: Parser Int
+countAs = length <$> many (char 'A')
+
+countAs' :: Parser Int
+countAs' = do            -- fmap nélkül, Monad instance-al?
+  cs <- many (char 'A')
+  return (length cs)
+
+------------------------------------------------------------
+
+-- regex-ek: típus Parser ()  (arra vagyunk kíváncsiak, hogy illeszkedik-e
+--                             egy regex az inputra)
+--
+--  Parser ()            regex
+--------------------------------
+--   char c               c
+--   string s             s      (karaktersorozat)
+--  p1 >> p2             p1p2
+--  p1 <|> p2            p1|p2
+--   many_ p              p*
+--   some_ p              p+
+--    eof                 $
 
 --------------------------------------------------------------------------------
 
@@ -145,7 +186,7 @@ p4 :: Parser ()
 p4 = undefined
 
 -- -?[0..9]+
--- (?e azt jelenti, hogy e opcionális)
+-- (e? azt jelenti, hogy e opcionális)
 p5 :: Parser ()
 p5 = undefined
 
