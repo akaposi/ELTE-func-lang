9bb72b6ea4183d78ec4c48ef82ccc971241d7c2b
Török Bálint Bence <torokbalint.school@gmail.com>
2024-10-09 11:25:47 +0200

    Zserbó gyak pre

--- 2024-25-1/2/Gy04.hs
+++ 2024-25-1/2/Gy04.hs
@@ -25,7 +25,11 @@ combineThrees f x y
 -- magicFunction 2 == Nothing (incrementIfEven 2 == 3, 2 + 3 `mod` 3 /= 0)
 
 magicFunction :: Integral a => a -> Maybe a
-magicFunction = undefined
+magicFunction x = case incrementIfEven x of
+  Nothing -> Nothing
+  (Just y) -> case combineThrees (*) x y of
+    Nothing -> Nothing
+    (Just k) -> Just (k ^ 2)
 
 -- Ez még egy darab Maybe vizsgálatnál annyira nem vészes, de ha sokat kell, elég sok boilerplate kódot vezethet be
 -- Az úgynevezett "mellékhatást" (tehát ha egy számítás az eredményen kívül valami mást is csinál, Maybe esetén a művelet elromolhat)
@@ -41,15 +45,17 @@ class Functor m => Monad m where
 -}
 -- A >>= (ún bind) művelet modellezi egy előző "mellékhatásos" számítás eredményének a felhasználását.
 -- Maybe esetén (>>=) :: Maybe a -> (a -> Maybe b) -> Maybe b
---                                   ^ csak akkot fut le ha az első paraméter Just a
+--                                   ^ csak akkor fut le ha az első paraméter Just a
 
 -- Bind, nagyon hasonít a függvény applikációra ($)
 -- (>>=) :: m a -> (a -> m b) -> m b
 -- ($)   ::   a -> (a ->   b) ->   b
 -- TODO : Miért
 
+main = print "Hello World!"
+
 magicFunctionM :: Integral a => a -> Maybe a
-magicFunctionM x = undefined
+magicFunctionM x = incrementIfEven x >>= combineThrees (*) x <$> (^ 2)
 
 -- Így lehet több olyan műveletet komponálni, amelyeknek vannak mellékhatásaik
 -- Akinek nem tetszik a >>= irogatás létezik az imperatív stílusú do notáció
@@ -62,7 +68,11 @@ y >>= \x -> a
 -}
 
 magicFunctionDo :: Integral a => a -> Maybe a
-magicFunctionDo = undefined
+magicFunctionDo x = do
+  y <- incrementIfEven x
+  -- let h = incrementIfEven x
+  k <- combineThrees (*) x y
+  return (k ^ 2)
 
 
 -- Monád példa: IO monád
@@ -85,28 +95,43 @@ print :: Show a => a -> IO ()
 -- d, beolvas egy számot minden listaelemhez és azt hozzáadja
 
 readAndConcat :: IO ()
-readAndConcat = undefined
+readAndConcat = getLine >>= 
+  \a -> getLine >>= 
+  \b -> putStrLn (a ++ b)
 
 readAndConcat' :: IO ()
-readAndConcat' = undefined
+readAndConcat' = do
+  a <- getLine
+  b <- getLine
+  putStrLn (a ++ b)
 
 readAndSq :: IO ()
-readAndSq = undefined
+readAndSq = readLn >>= \num -> print (num ^ 2)
 
 readAndSq' :: IO ()
-readAndSq' = undefined
+readAndSq' = do
+  (num :: Integer) <- readLn
+  print (num ^ 2)
 
 printAll :: Show a => [a] -> IO ()
-printAll = undefined
+printAll = print
 
 printAll' :: Show a => [a] -> IO ()
-printAll' = undefined
+printAll' ls = do
+  print ls
 
 readAndAdd :: (Read a, Num a) => [a] -> IO [a]
-readAndAdd = undefined
+readAndAdd [] = return []
+readAndAdd (a : ls) = readLn >>= 
+  \num -> readAndAdd ls >>= 
+  \nls -> return ((num + a) : nls)
 
 readAndAdd' :: (Read a, Num a) => [a] -> IO [a]
-readAndAdd' = undefined
+readAndAdd' [] = return []
+readAndAdd' (a : ls) = do
+  num <- readLn
+  nls <- readAndAdd' ls 
+  return $ (a + num ) : nls
 
 -- Monád példa: Állapotváltozás monád (State monád)
 --                          v rekord szintaxis, ekvivalens azzal hogy State (s -> (s,a))
@@ -128,7 +153,7 @@ get = State $ \s -> (s,s)
 
 -- Felülírja a jelenlegi állapotot
 put :: s -> State s ()
-put s = State $ const ((), s)
+put s = State $ const ((), s) -- State $ \_ -> ((), s)
 
 -- runState :: State s a -> s -> (a,s)
 -- lefuttat egy állapotváltozást egy adott kezdeti állapotra
@@ -149,12 +174,18 @@ modify f = State $ \s -> ((), f s)
 -- Minden állapotválotzást megírható >>=, return, get és put segítségével
 -- Írjuk meg bindal/do notációval az incrementAndEven állapotváltozást
 incrementAndEvenBind :: State Int Bool
-incrementAndEvenBind = undefined
+incrementAndEvenBind = get >>= \i -> put (i +1) >> return (even i)
 
 incrementAndEvenDo :: State Int Bool
-incrementAndEvenDo = undefined
+incrementAndEvenDo = do
+  i <- get
+  put (i + 1)
+  return $ even i
 
 -- runState-el lehet tesztelni, pl runState incrementAndEvenBind 3 == (False, 4)
+
+-- >>> runState incrementAndEvenBind 3 == (False, 4)
+-- True
 
 -- Definiáljunk állapotváltozásokat mely
 -- a, leszedi az állapotbeli lista fejelemét ha van olyan
@@ -163,16 +194,89 @@ incrementAndEvenDo = undefined
 -- d, összeadj az állapotbeli lista összes elemét és kiűríti azt (csak a primitív kombinátorokat és az a,-t használd)
 
 behead :: State [a] (Maybe a)
-behead = undefined
+behead = get >>= \ls -> case ls of
+  [] -> return Nothing
+  (a : ls) -> put ls >> return (Just a)
+
+behead' :: State [a] (Maybe a)
+{-
+behead' = do
+  ls <- get
+  case ls of
+    [] -> return Nothing
+    (a : ls) -> do
+      put ls
+      return $ Just a
+-}
+
+behead' = do {
+  ls <- get;
+  case ls of
+    [] -> return Nothing;
+    (a : ls) -> do {
+      put ls;
+      return $ Just a ;
+    }
+}
 
 takeFromSt :: Integral i => i -> State [a] [a]
-takeFromSt = undefined
+takeFromSt i | i <= 0 = return []
+takeFromSt i          = get >>= \ls -> case ls of
+  [] -> return []
+  (a : ls) -> put ls >>
+    takeFromSt (i - 1) >>=
+    \nls -> return $ a : nls
+
+
+takeFromSt' :: Integral i => i -> State [a] [a]
+takeFromSt' i | i <= 0 = return []
+takeFromSt' i  = do
+  ls <- get
+  case ls of
+    [] -> return []
+    (a : ls) -> do
+      put ls
+      nls <- takeFromSt' (i - 1)
+      return $ a : nls
 
 takeWhileFromSt :: (a -> Bool) -> State [a] [a]
-takeWhileFromSt = undefined
+takeWhileFromSt p = get >>= \ls -> case ls of
+  [] -> return []
+  (a : ls) -> case p a of
+    False -> return []
+    True -> put ls >>
+      takeWhileFromSt p >>= 
+      \nls -> return $ a : nls
+
+takeWhileFromSt' :: (a -> Bool) -> State [a] [a]
+takeWhileFromSt' p = do
+  ls <- get
+  case ls of
+    [] -> return []
+    (a : ls) -> case p a of
+      False -> return []
+      True -> do
+        put ls
+        nls <- takeWhileFromSt' p
+        return $ a : nls
 
 summing :: Num a => State [a] a
-summing = undefined
+summing = get >>= \ls -> case ls of
+  [] -> return 0
+  (a : ls) -> put ls >>
+    summing >>=
+    \sum -> return $ a + sum
+
+summing' :: Num a => State [a] a
+summing' = do
+  ls <- get
+  case ls of
+    [] -> return 0
+    (a : ls) -> do
+      put ls
+      sum <- summing'
+      return $ sum + a
+
 
 -- Definiáljuk egy fa preorder, postorder és inorder címkézését állapotváltozásokkal
 -- Az állapotban a legutoljára kiadott indexet tárolja
@@ -180,7 +284,8 @@ summing = undefined
 data Tree a = Leaf a | Node (Tree a) (Tree a) deriving (Eq, Show)
 
 preorder :: Tree a -> Tree (a, Int)
-preorder = undefined
+preorder a = fst $ flip runState 0 $ do
+  _
 
 postorder :: Tree a -> Tree (a, Int)
 postorder = undefined
