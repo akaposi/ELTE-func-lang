a6bdf9160f16deae5e3462d500b6afa2192d2f9c
moder <korpa.peter0723@gmail.com>
2025-03-01 11:07:39 +0100

    gy03 post/gy04 pre

--- 2024-25-2/2/Gy03.hs
+++ 2024-25-2/2/Gy03.hs
@@ -3,6 +3,17 @@ module Gy03_pre where
 
 import Prelude hiding (Maybe(..), Either(..))
 
+-- Írjuk meg azt a függvényt ami összeszorozza a [Int] elemeit
+
+multiply :: [Int] -> Int
+multiply []     = 1
+multiply (x:xs) = x * multiply xs
+
+
+multiply' :: [Int] -> Int
+multiply' xs = foldr (*) 1 xs
+
+
 -- Hajtogatás: Listára való rekurzió szimulációja
 -- foldr :: (a ->  b  ->  b ) ->  b -> [a] -> b
 --   (:) :: (a -> [a] -> [a])
@@ -10,6 +21,7 @@ import Prelude hiding (Maybe(..), Either(..))
 --                         [] :: [a]
 -- A hajtogatás összekombinálja az összes 'a' típusú kifejezést egy listában.
 -- Próbáljuk ezt meg más típussal is eljátszani:
+
 
 data Single a = Single a deriving (Eq, Show)
 data Tuple a = Tuple a a deriving (Eq, Show)
@@ -31,19 +43,73 @@ data Prod f a b = FProd (f a) (f b) deriving (Eq, Show)
 data FList f a = FNil | FCons (f a) (f (FList f a))
 
 foldrSingle :: (a -> b -> b) -> b -> Single a -> b
-foldrSingle = undefined
+foldrSingle f b (Single a) = f a b
 
 foldrTuple :: (a -> b -> b) -> b -> Tuple a -> b
-foldrTuple = undefined
+foldrTuple f b (Tuple a a') = f a (f a' b)
+
+{-
+
+f a (f a' b)
+
+     f
+    / \
+  a   f
+     / \
+    a'  b
+-}
 
 foldrQuintuple :: (a -> b -> b) -> b -> Quintuple a -> b
 foldrQuintuple = undefined
 
 foldrList :: (a -> b -> b) -> b -> List a -> b
-foldrList = undefined
+foldrList f b Nil         = b
+foldrList f b (Cons x xs) = f x (foldrList f b xs)
+
+{-
+Vezessük le:
+foldrList (+) 0 (Cons 1 $ Cons 2 Nil) = 
+(+) 1 (foldrList (+) 0 (Cons 2 Nil))  =
+(+) 1 ((+) 2 (foldrList (+) 0 Nil))   =
+(+) 1 ((+) 2 (0))   =
+
+     (+)
+    /  \
+   1   (+)
+      /  \ 
+     2    0
+-}
+
+foldlList :: (b -> a -> b) -> b -> List a -> b
+foldlList f b Nil         = b
+foldlList f b (Cons x xs) = foldlList f (f b x) xs
+ 
+
+{-
+Vezessük le ezt is:
+foldlList (+) 0 (Cons 1 $ Cons 2 Nil) =
+foldlList (+) ((+) 0 1) (Cons 2 Nil)  =
+foldlList (+) ((+) ((+) 0 1) 2) (Nil)  =
+((+) ((+) 0 1) 2)  =
+
+    (+)
+   /   \
+  (+)   2
+ /   \
+0    1
+
+-}
+
+-- Teszt : 
+-- foldr (^) 1 [2,1,3] == 2 == 
+-- 
+-- foldl (^) 1 [2,1,3] == 1 == 
+-- 
 
 foldrMaybe :: (a -> b -> b) -> b -> Maybe a -> b
-foldrMaybe = undefined
+foldrMaybe f b Nothing = b
+foldrMaybe f b (Just a) = f a b
+
 
 -- Hasonlóan a mappolhatósághoz, a hajtogatás is általánosítható a Foldable típusosztály segítéségvel
 {-
@@ -96,11 +162,29 @@ instance Foldable Maybe where
   foldr :: (a -> b -> b) -> b -> Maybe a -> b
   foldr = foldrMaybe
 
+{-
+data NonEmpty a = Last a | NECons a (NonEmpty a) deriving (Eq, Show)
+data NonEmpty2 a = NECons2 a (List a) deriving (Eq, Show)
+data Tree a = Leaf a | Node (Tree a) a (Tree a) deriving (Eq, Show)
+-}
+
 instance Foldable NonEmpty where
+  foldr f b (Last a) = f a b
+  foldr f b (NECons a ne) = f a (foldr f b ne)
+  
+
 
 instance Foldable NonEmpty2 where
+  foldr f b (NECons2 a ne) = f a (foldr f b ne)
+--                               \-----------/
+--                                     ^- itt listákra foldr-elünk
+--  ne :: List a
 
 instance Foldable Tree where
+  foldr f b (Leaf a) = f a b
+  foldr f b (Node l a r) = foldr f (f a (foldr f b r)) l
+
+
 
 instance Foldable (Either fixed) where
 
@@ -111,9 +195,17 @@ instance Foldable (TriEither fixed1 fixed2) where
 instance Foldable (BiList fixed) where
 
 -- Magasabbrendű megkötések
-instance Foldable f => Foldable (Apply f) where
 
+--data Apply f a = MkApply (f a) deriving (Eq, Show)
+instance Foldable f => Foldable (Apply f) where
+  foldr f b (MkApply fa) = (foldr f b fa)
+
+-- data Fix f a = MkFix (f (Fix f a))
 instance Foldable f => Foldable (Fix f) where
+  foldr f b (MkFix fFixfa) = foldr (\fixfa b' -> foldr f b' fixfa) b fFixfa
+--                                   flip (foldr f)
+
+-- Eddig jutottunk
 
 instance (Foldable f, Foldable g) => Foldable (Compose f g) where
 
@@ -129,9 +221,13 @@ instance Foldable f => Foldable (FList f) where
 Félcsoport: Olyan H halmaz, amely rendelkezik egy <> asszociatív művelettel
 Ez Haskellben a Semigroup típusosztály
 
+https://en.wikipedia.org/wiki/Magma_(algebra)#/media/File:Magma_to_group4.svg
+
 Írjunk Semigroup instance-ot az alábbi típusokra!
 
 -}
+
+
 
 instance Semigroup Bool where
   (<>) :: Bool -> Bool -> Bool
@@ -139,7 +235,7 @@ instance Semigroup Bool where
 
 instance Semigroup Int where
   (<>) :: Int -> Int -> Int
-  (<>) = undefined
+  (<>) = (+)
 
 data Endo a = MkEndo (a -> a)
 
@@ -147,13 +243,14 @@ instance Semigroup (Endo a) where
   (<>) :: Endo a -> Endo a -> Endo a
   (<>) = undefined
 
+
 {-
 Egy halmazhoz több művelet is választható, hogy félcsoportot alkossanak
 Pl.:
 (ℝ, +)         egy félcsoport
 (ℝ, *)         egy félcsoport
-(A, \x y -> x) egy félcsoport
-(A, \x y -> y) egy félcsoport
+(A, λx y -> x) egy félcsoport
+(A, λx y -> y) egy félcsoport
 -}
 
 {-
