15a1e407cd3d759d4146f586b37cebf78dfdb938
moder <korpa.peter0723@gmail.com>
2025-03-31 12:22:35 +0200

    gyak 7 after

--- 2024-25-2/2/Gy07_pre.hs
+++ 2024-25-2/2/Gy07_pre.hs
@@ -6,6 +6,112 @@ import Control.Monad.Writer
 import Control.Monad.Except
 import Control.Monad.IO.Class
 
+-- A State monád állapot változást reprezentált
+-- Vegyünk három új Monádot:
+
+
+-- Reader: olvasási környezet, például globális konstans
+-- newtype Reader r a = Reader { runReader :: r -> a }
+--                                                ^ state-nek elhagytuk a kimeneti paraméterét
+
+-- Primitív műveletek
+
+-- ask :: Reader r r
+-- ask = Reader id
+-- Visszaadja környezetet, ugyanaz mint a get state-nél
+
+data Env = MkEnv { homeDir :: String, isAdmin :: Bool }
+
+canWriteHere :: String -> Reader Env Bool
+canWriteHere path = do
+  MkEnv homeDir adm <- ask
+  return (adm || path == homeDir)
+
+-- local :: (r -> r) -> Reader r a -> Reader r a
+-- Lokális megváltoztatja a környezetet a második paraméterben
+
+sudo :: Reader Env () -> Reader Env ()
+sudo doas = do
+  MkEnv homeDir adm <- ask
+  when adm $ local (const (MkEnv "/root" True)) doas
+
+-- Feladatok
+
+
+
+-- Írjunk egy olyan map függvényt reader segítségével, amely az egyes listaelemek indexével is összekombinálja az elemeket.
+-- Az olvasási környezetben tároljuk a jelenlegi indexet
+mapWithIndex :: Integral i => (i -> a -> b) -> [a] -> [b]
+mapWithIndex f xs = runReader (mwiReader f xs) 0
+
+mwiReader :: Integral i => (i -> a -> b) -> [a] -> Reader i [b]
+mwiReader = undefined
+
+
+-- Writer: írási környezet, például loggingra hasznos
+-- newtype Writer w a = Writer { runWriter :: (a, w) }
+--                                           ^ state-nek elhagytuk a bemeneti paraméterét
+-- Primitív műveletek
+
+-- tell :: Monoid w => w -> Writer w () -- üzenet írása, a >>= kombinálja az ezzel írt üzeneteket <>-vel
+-- tell w = Writer (w, ())
+-- ugyanaz mint a put State-nél
+
+calculation :: Writer [String] Int
+calculation = do
+  tell ["1-esel kezdünk"]
+  let x = 1
+  tell ["Aztán egy 2-es"]
+  let y = 2
+  tell ["Majd az összeg", "Egyszerre többet is tud loggolni"]
+  return (x + y)
+
+--                         v lefuttatja ezt a writert
+-- listen :: Monoid w => Writer w a -> Writer w (a, w) -- és visszaadja a loggolásait
+-- listen (Writer res) = Writer (res, res)
+
+calculation2 :: Writer [String] Int
+calculation2 = do
+  tell ["Na vágjunk bele"]
+  (res, messages) <- listen calculation
+  tell ["Elhagyjuk a résszámolás első üzenetét", "A többit ismét reportáljuk"]
+  tell (tail messages)
+  tell ["Majd eredmény + 1"]
+  return (res + 1)
+
+--                                v alkalmazza a függvényt a saját kiírandó szövegeire
+-- pass :: Monoid w => Writer w (a, w -> w) -> Writer a
+-- pass (Writer w (a, f)) = Writer (f w) a
+
+silence :: Monoid w => Writer w a -> Writer w a
+silence w = pass $ do
+  a <- w
+  return (a, const [])
+
+-- Except: hibakörnyezet, képes hibakezelésre
+-- newtype Except e a = Except { runExcept :: Either e a }
+--                ^ az "e" hibát jelez, ha valaha Left e lesz belül, a bind-ok több műveletet nem tud elvégezni, tehát megáll az egész hamarabb
+
+-- Primitív műveletek
+
+-- throwError :: e -> Except e a
+-- throwError e = Except (Left e)
+-- hibát dob
+
+tryDiv :: Int -> Int -> Except String Int
+tryDiv x y = do
+  when (y == 0) $ throwError "0-val való osztás"
+  return (div x y)
+
+
+--                                v ha hiba történik, ezt lefuttatja
+-- catchError :: Except e a -> (e -> Except e a) -> Except e a
+-- catchError (Except (Left e)) f = f e
+-- catchError x _ = x
+
+runCalc :: Except String Int
+runCalc = catchError (tryDiv 1 0) $ \_ -> return 11
+
 
 -- Cheatsheet:
 {-
@@ -37,7 +143,6 @@ Mindenhol az eredményt egy tetszőleges monádba csomagoljuk, így tudjuk neste
 A standard libraryben lévő primitív műveletek a nestelt monádokat is megtalálják
 -}
 
-data Env = MkEnv { homeDir :: String, isAdmin :: Bool }
 
 --                         v itt azért nem WriterT van, mert több mellékhatást nem akarunk belerakni
 adminCheck :: ReaderT Env (Writer [String]) Bool
