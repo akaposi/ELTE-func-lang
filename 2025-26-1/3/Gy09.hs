77390a7fc7c5bba8a13e7fdd1c5ed4266b47e5c5
Márton Petes <tx0lwm@inf.elte.hu>
2025-11-11 19:14:45 +0100

    gy9

--- 2025-26-1/3/Gy09.hs
+++ 2025-26-1/3/Gy09.hs
@@ -31,39 +31,56 @@ instance Monad Parser where
 -- Primitív parserek
 -- Olyan parser, amely lenyel egy karaktert a bemenetről és akkor fogad el, ha az adott predikátum teljesül rá
 satisfy :: (Char -> Bool) -> Parser Char
-satisfy = undefined
+satisfy p = Parser $ \inputString -> case inputString of
+  [] -> Nothing
+  (c:cs) -> if p c then Just (c, cs) else Nothing
 
 -- Olyan parser, amely akkor fogad el, ha nincs semmi a bemeneten
 eof :: Parser ()
-eof = undefined
+eof = Parser $ \is -> case is of
+  [] -> Just ((), []) -- pure []
+  _  -> Nothing
 
 -- Ezekből felépíthetőek egyéb parserek
 -- Olyan parser, ami egy adott karaktert parseol
 -- Itt irreleváns az, hogy mi a kimenet
 char :: Char -> Parser ()
-char = undefined
+char c = do -- void $ satisfy (== c)
+  satisfy (== c)
+  return ()
 
 -- Parseoljunk akármilyen karaktert
 anychar :: Parser Char
-anychar = undefined
+anychar = satisfy (const True)
 
 -- Parseoljunk egy konkrét stringet
 -- hint: mapM_
 string :: String -> Parser ()
-string = undefined
+string [] = return ()
+string (x:xs) = do
+  char x
+  string xs
+
+-- string i = mapM_ char i
+-- string i = void (mapM char i)
+
 
 -- Parsernél fontos a "vagy" művelet (ha az első parser elhasal, akkor a másikat próbáljuk meg)
 -- Ez lesz az Alternative típusosztály
 instance Alternative Parser where
   empty :: Parser a -- Garantáltan elhasaló parser
-  empty = undefined
+  empty = Parser $ const Nothing
+  
   (<|>) :: Parser a -> Parser a -> Parser a -- Ha a baloldali sikertelen, futassuk le a jobboldalit (hint: A Maybe is egy alternatív)
-  (<|>) = undefined
+  p1 <|> p2 = Parser $ \iS -> case runParser p1 iS of
+    Nothing -> runParser p2 iS
+    x       -> x
 
 -- Definiáljunk egy parsert ami egy 'a' vagy egy 'b' karaktert parseol
 
 aorb :: Parser Char
-aorb = undefined
+aorb = satisfy (== 'a') <|> satisfy (== 'b')
+  -- ('a' <$ char 'a') <|> ('b' <$ char 'b')
 
 -- many :: Parser a -> Parser [a]
 -- 0 vagy többször lefuttatja a parsert
@@ -79,28 +96,45 @@ some' p = (:) <$> p <*> many' p -- Lefuttatja 1x és utána 0 vagy többször
 -- optional' :: Parser a -> Parser (Maybe a)
 -- ha elhasalna a parser, mégse hasal el
 optional' :: Parser a -> Parser (Maybe a)
-optional' p = undefined
+optional' p = do
+  a <- (Just <$> p) <|> return Nothing
+  return a
 
 -- replicateM :: Int -> Parser a -> Parser [a]
 -- n-szer lefuttat egy parsert
 replicateM' :: Integral i => i -> Parser a -> Parser [a]
-replicateM' = undefined
+replicateM' 0 p = return [] -- pure []
+replicateM' i p = (:) <$> p <*> replicateM' (i - 1) p
+
+-- <$> = fmap
+-- <*> = ap
+-- <|> = alt
+-- <$ = const fmap
+-- <* = rap
+-- *> = lap
+-- >>= = bind
+-- >> = const bind
+
+
 
 -- asum :: [Parser a] -> Parser a
 -- Sorban megpróbálja az összes parsert lefuttatni
 asum' :: [Parser a] -> Parser a
-asum' = undefined
+asum' [] = empty
+asum' (p:ps) = p <|> asum' ps -- foldr (<|>) empty 
 
 -- Regex féle parserek
 {-
     Regex gyorstalpaló:                               Haskell megfelelő:
     c        - Parseol egy c karaktert                char 'c'
+    s        - Parseol egy s stringet                 string s
     ℓ+       - Parseol 1 vagy több ℓ kifejezést       some ℓ
     ℓ*       - Parseol 0 vagy több ℓ kifejezést       many ℓ
     (ℓ₁|ℓ₂)  - Parseol ℓ₁-t vagy ℓ₂-t                 ℓ₁ <|> ℓ₂
     ℓ?       - Parseol 0 vagy 1 ℓ kifejezést          optional ℓ
-    .        - Akármilyen karakter                    anyChar
+    .        - Akármilyen karakter                    anychar
     ℓ{n}     - Parseol n darab ℓ kifejezést           replicateM n ℓ
+    ℓ{n,}    - Parseol n vagy több ℓ kifejezést       replicateM n ℓ >> many ℓ 
     $        - Nincs mit parseolni                    eof
     \d       - Parseol egy számjegyet                 digitToInt <$> satisfy isDigit
     [c₁-c₂]  - c₁ és c₂ között parseol egy karaktert  satisfy (\x -> x >= min c₁ c₂ && x <= max c₁ c₂)
@@ -110,23 +144,35 @@ asum' = undefined
 
 -- alm(a|ák)
 p1 :: Parser ()
-p1 = undefined
+p1 = do
+  string "alm"
+  char 'a' <|> string "ák"
 
 -- c(i+)ca
 p2 :: Parser ()
-p2 = undefined
+p2 = do
+  char 'c'
+  some (char 'i')
+  string "ca"
 
 -- (c*)i?(c+)a{3}
 p3 :: Parser ()
-p3 = undefined
+p3 = do
+  many (char 'c')
+  optional (char 'i')
+  some (char 'c')
+  replicateM' 3 (char 'a')
+  return ()
 
 -- (alma|banana)?
 p4 :: Parser ()
-p4 = undefined
+p4 = void $ optional $ string "alma" <|> string "banana"
 
 -- [A-Z]{10}
 p5 :: Parser ()
-p5 = undefined
+p5 = do
+  replicateM' 10 (asum' [char c | c <- ['A'..'Z']])
+  pure ()
 
 -- \d{2}
 p6 :: Parser ()
@@ -148,7 +194,7 @@ p9 = undefined
 p10 :: Parser ()
 p10 = undefined
 
--- [A-Z]*[1,2,3,9]?(A|Z)$
+-- [A-Z]*(1|2|3|9)?(A|Z)$
 p11 :: Parser ()
 p11 = undefined
 
@@ -158,4 +204,10 @@ p12 = undefined
 
 -- alm(a|ák)|banán(ok)?
 p13 :: Parser ()
-p13 = undefined
+p13 = (do -- string "alm" >> (char 'a' <|> string "ák")
+          string "alm"
+          char 'a' <|> string "ák"
+      ) <|> (do -- string "banán" >> (void $ optional (string "ok"))
+                string "banán"
+                void $ optional (string "ok")
+                )
