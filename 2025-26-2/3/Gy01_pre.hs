a70571535056807cae860a1cba5ea12b436edf9e
Stefania Damato <psxsd7@nottingham.ac.uk>
2026-02-10 13:51:11 +0100

    Practice class 1

---
+++ 2025-26-2/3/Gy01_pre.hs
@@ -0,0 +1,184 @@
+{-# OPTIONS_GHC -Wincomplete-patterns #-}
+
+module Gy01 where
+
+{-
+
+Topics:
+- The longer version is available on GitHub
+- Requirements:
+  - Weekly homework assignments, each with a 2-week deadline, mandatory
+  - 3 major assignments during the semester (3 * 4 points), not mandatory. If you earn 8 points, you will receive +1 grade on the exam if you have a 2.
+  - During the exam period, there will be an exam covering the entire semester's practical material.
+  - Lecture: Tuesday 10:00
+  - You may miss a maximum of 3 practical classes
+
+- Any IDE and software may be used for the course (VSCode, Emacs, Neovim, etc.), including the Haskell Language Server
+- Anything may be used during the exam, except for human assistance and AI
+
+- Class files: https://github.com/Akaposi/ELTE-func-lang/tree/master/2025-26-2/gyakX
+- GyXX_pre.hs = File before class
+- GyXX.hs     = File after class
+
+- The course is based on the Functional Programming (IP-18FUNPEG) course
+- For those who are behind: lambda.inf.elte.hu
+
+GHCi reminder:
+- :l <file>     - loads the file into GHCi
+- :r            - reloads the loaded files
+- :bro <module> - short for browse, prints the contents of a module
+- :t <expr>     - prints the type of an expression
+- :i <that>     - prints information about an fv/type/etc. (binding strength, where it is defined, etc.)
+- :set <flag>   - turns on a flag (e.g., -Wincomplete-patterns)
+- :q            - exit
+
+Pragmas:
+{-# <PRAGMA> <OPTIONS> #-}
+- This always goes at the top of the file
+- Important pragmas:
+  - OPTIONS_GHC: turns on GHC flags, e.g. -Wincomplete-patterns, which gives a warning if a pattern matching is not total
+  - LANGUAGE: Turns on language extensions, e.g. InstanceSigs, which allows explicit typing of instance functions
+
+-}
+
+
+-- Today's topic: Recalling functional programing (functions, pattern matching, algebraic data types, type systems)
+xor :: Bool -> Bool -> Bool
+xor x y = undefined
+
+-- There may be multiple solutions (pattern matching, builtin functions)
+-- New "case" expression
+{-
+case x of
+  True -> ...
+  False -> ...
+-}
+
+-- Let/Where expression: local definitions:
+twelve :: Int
+twelve = x + x
+  where
+    x = 6
+
+twelve' :: Int
+twelve' = let x = 6 in x + x
+
+-- Polymorphism: The function works for any type arguments, as long as its consistent throughout the function
+id' :: a -> a
+id' x = x
+
+-- There can be multiple type variables
+f1 :: (a, (b, (c, d))) -> (b, c)
+f1 = undefined
+
+-- Help: Hole technology!
+-- In Haskell, if we write _ on the right side of the equality, the compiler tells us what type of expression is needed there
+
+-- There are multiple solutions for every function (e.g., with built-in functions)
+
+f2 :: (a -> b) -> a -> b
+f2 = undefined
+
+f3 :: (b -> c) -> (a -> b) -> a -> c
+f3 = undefined
+
+f4 :: (a -> b -> c) -> b -> a -> c
+f4 = undefined
+
+-- Helper functions:
+-- fst :: (a,b) -> a
+-- snd :: (a,b) -> b
+
+f5 :: ((a, b) -> c) -> (a -> (b -> c)) -- a -> b -> c == a -> (b -> c) due to Currying
+f5 = undefined
+
+f6 :: (a -> b -> c) -> (a, b) -> c
+f6 = undefined
+
+-- If the solution requires you to supply a function, use lambdas
+-- eg.: \x -> x
+
+f7 :: (a -> (b, c)) -> (a -> b, a -> c)
+f7 = undefined
+
+f8 :: (a -> b, a -> c) -> (a -> (b, c))
+f8 = undefined
+
+-- ADT reminder:
+-- Either data type. It has two constructors, Left and Right, which store either a or b:
+{-
+:i Either
+data Either a b = Left a | Right b
+-}
+
+f9 :: Either a b -> Either b a
+f9 = undefined
+
+f10 :: (Either a b -> c) -> (a -> c, b -> c)
+f10 = undefined
+
+f11 :: (a -> c, b -> c) -> (Either a b -> c)
+f11 = undefined
+
+-- Bonus
+
+f12 :: Either (a, b) (a, c) -> (a, Either b c)
+f12 = undefined
+
+f13 :: (a, Either b c) -> Either (a, b) (a, c)
+f13 = undefined
+
+f14 :: (a -> a -> b) -> ((a -> b) -> a) -> b
+f14 = undefined
+
+-- List reminder
+-- How is the list defined?
+
+-- Define the map and filter functions with a list generator, recursion, and folding.
+
+map' :: (a -> b) -> [a] -> [b]
+map' = undefined
+
+filter' :: (a -> Bool) -> [a] -> [a]
+filter' = undefined
+
+
+-- Let's define some other useful list functions that are part of the standard library.
+-- !! It is not recommended to reinvent the entire Haskell stdlib for the exam !!
+
+take', drop' :: Int -> [a] -> [a]
+
+take' = undefined
+drop' = undefined
+
+splitAt' :: Int -> [a] -> ([a], [a])
+splitAt' = undefined
+
+takeWhile', dropWhile' :: (a -> Bool) -> [a] -> [a]
+
+takeWhile' = undefined
+dropWhile' = undefined
+
+span', partition' :: (a -> Bool) -> [a] -> ([a], [a])
+
+span' = undefined
+partition' = undefined
+
+zipWith' :: (a -> b -> c) -> [a] -> [b] -> [c]
+zipWith' = undefined
+
+cycle' :: [a] -> [a]
+cycle' = undefined
+
+iterate' :: a -> (a -> a) -> [a]
+iterate' = undefined
+
+repeat' :: a -> [a]
+repeat' = undefined
+
+replicate' :: Int -> a -> [a]
+replicate' = undefined
+
+nub' :: Eq a => [a] -> [a]
+nub' = undefined
+
