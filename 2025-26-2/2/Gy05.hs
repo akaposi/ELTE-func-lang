968907ce3cdb099176bf3851e98b61884ea783a2
Suszter Roland <xjq2db@inf.elte.hu>
2026-03-17 10:11:11 +0100

    gy06

--- 2025-26-2/2/Gy05.hs
+++ 2025-26-2/2/Gy05.hs
@@ -23,19 +23,21 @@ import Control.Monad.Except
 --, hogy ha egy Jegyet bele rakunk, zölden világít és a Nyitva állapotra váltunk
 
 -- Definiáljuk az állapotok típusát
-data MachineState
+data MachineState = Closed | Open
   deriving (Eq, Show)
 
 -- Definiáljuk a fények típusát
-data LightColour
+data LightColour = Green | Yellow | Red
   deriving (Eq, Show)
 
 
 -- Definiáljuk az átmenetek függvényeit
 -- A függvények egy kezdeti állapotból egy végállapotba és egy világító fénybe képeznek
 push, insertTicket :: MachineState -> (LightColour, MachineState)
-push = undefined
-insertTicket = undefined
+push Closed = (Red, Closed)
+push Open = (Green, Closed)
+insertTicket Closed = (Green, Open)
+insertTicket Open = (Yellow, Open)
 
 -- Ennek a segítségével például le tudjuk modellezni, hogy Pistike 2x próbál jegy nélkül bemenni
 pistike :: MachineState -> ([LightColour], MachineState)
@@ -74,7 +76,7 @@ háttérben a -> s -> (a,s)
 -- s -> (a,s) függvényeket a state függvénnyel lehet becsomagolni
 pushS, insertTicketS :: State MachineState LightColour
 pushS = state push
-insertTicketS = undefined
+insertTicketS = state insertTicket
 
 -- Így pistikét kicsit szebben lehet definiálni
 pistikeS :: State MachineState [LightColour]
@@ -109,31 +111,73 @@ countYellow = do
 -- Gerike: Tol, Jegy, Tol, Tol
 -- Gerike esetén azt adjuk vissza, hányszor 'Piros' volt az átmenetek eredménye
 
+janika :: State MachineState [LightColour]
+janika = do
+  l1 <- insertTicketS
+  l2 <- insertTicketS
+  l4 <- pushS
+  return [l1, l2, l4]
+
+gerike :: State MachineState Int
+gerike = do
+  l1 <- pushS
+  l2 <- insertTicketS
+  l3 <- pushS
+  l4 <- pushS
+  return $ length $ filter (== Red) [l1, l2, l3, l4]
 
 -- Komplikáltabb feladatok
 -- Implementáljunk egy 'get' műveletet, amely visszaadja az állapotot
 get' :: State s s
-get' = undefined
+get' = state (\s -> (s,s))
 -- Implementáljunk egy 'put' műveletet, amely felülírja az állapotot
 put' :: s -> State s ()
-put' = undefined
+put' s = state $ const ((), s)
 
 -- Ezek után nem kell a 'state' függvénnyel szórakozni
 
 -- Példa get/putra: Definiáljuk a safeHead függvényt ami az állapotban lévő lista fejelemét leszedi - ha van neki.
 pop :: State [a] (Maybe a)
-pop = undefined
+pop = do
+  l <- get
+  case l of
+    (x : xs) -> put xs >> return (Just x)
+    [] -> return Nothing
+
 
 -- Példa get/putra 2: Definiáljuk a take függvényt a belső állapotra (esetleg pop-ot is lehet használni).
 takeK :: Int -> State [a] [a]
-takeK = undefined
+takeK 0 = return []
+takeK n = do
+  l <- get
+  case l of
+    [] -> return []
+    (x : xs) -> do
+      put xs
+      r <- takeK (n-1)
+      return (x : r)
+
+-- modify f = get >>= \s -> put (f s)
 
 -- Definiáljuk az alábbi függvényeket!
 popLast :: State [a] (Maybe a) -- leszedi az utolsó elemet a listából - ha van.
-popLast = undefined
+popLast = do
+  l <- get
+  case l of
+    [] -> return Nothing
+    [x] -> put [] >> return (Just x)
+    (x : xs) -> do
+      put xs
+      r <- popLast
+      {-
+      ls <- get
+      put (x : ls)
+      -}
+      modify (x :)
+      return r
 
 sumK :: Num a => Int -> State [a] a -- Kiszedi és összeadja az első K elemet a listából
-sumK = undefined
+sumK n = takeK n >>= \l -> return $ sum l
 
 labelList :: [a] -> State Int [(a, Int)] -- Minden elemet megcímkéz, a belső állapot a számláló
 labelList = undefined
@@ -149,12 +193,13 @@ labelListBW = undefined
 -- 0-val való osztás hiba
 -- Asszertációs hiba
 -- Üres lista hiba
-data CustomError
+data CustomError = DivByZero | AssertionError | EmptyList
   deriving (Eq, Show)
 
 -- Példa: biztonságos osztás
 safeDiv :: Integral a => a -> a -> Either CustomError a
-safeDiv = undefined
+safeDiv _ 0 = Left DivByZero
+safeDiv x y= Right $ div x y
 
 -- Az either-t is lehet monadikusan kezelni
 bindE :: Either e a -> (a -> Either e b) -> Either e b
@@ -163,7 +208,8 @@ bindE (Right a) f = f a
 
 -- Hibák dobása mellet el is kell tudni kapni őket:
 catchE :: Either e a -> (e -> Either e a) -> Either e a
-catchE = undefined
+catchE (Left e) f = f e
+catchE x _ = x
 
 -- Az Eithernek van kicsit általánosabb formája (részletesebben következő órán)
 -- Ez az Except Monád
@@ -176,15 +222,17 @@ catchE = undefined
 -- Példa throwError/catchErrorra:
 -- Dobjunk asszertációs hibát, ha a feltétel nem teljesül
 assert :: Bool -> Except CustomError ()
-assert = undefined
+assert False = throwError AssertionError
+assert True = return ()
 
 -- Biztonságos osztás
 safeDivE :: Integral a => a -> a -> Except CustomError a
-safeDivE = undefined
+safeDivE _ 0 = throwError DivByZero
+safeDivE x y = return $ div x y
 
 -- Végezzük el a safeDiv műveletet, de ha kivételt dobnánk, adjunk vissza 0-t inkább
 safeDivF :: Integral a => a -> a -> Except CustomError a
-safeDivF = undefined
+safeDivF x y = catchError (safeDivE x y) (const $ return 0)
 
 -- Feladatok
 
---
+++ 2025-26-2/2/Gy06.hs
@@ -0,0 +1,172 @@
+{-# LANGUAGE DerivingVia, DerivingStrategies #-}
+
+module Gy06 where
+
+import Control.Monad.State
+import Control.Monad.Reader
+import Control.Monad.Writer
+import Control.Monad.Except
+import Control.Monad.IO.Class
+import Data.Semigroup
+import Data.List
+
+-- Tegyük fel, hogy rendszergazdák vagyunk és Haskellben akarunk permission rendszert írni
+-- Ehhez mi el akarjuk tárolni, melyik felhasználónak mi a home directoryja
+
+type User = String
+data UserInfo = MkUserInfo { getHomeDirectory :: String, isAdmin :: Bool } deriving (Eq, Show)
+type Environment = [(User, UserInfo)] -- nevekhez asszociált home direcory és admin infó
+
+-- A környezet a műveletek során nem változik, ezért kimeneti környezet nem szükséges
+-- Definiáljunk egy függvényt ami egy adott nevű felhasználó home directoryját lekéri
+
+homeDirOf :: String -> Environment -> Maybe String
+homeDirOf = undefined
+
+-- Definiáljuk egy függvényt amely lekéri az összes admin felhasználó home directoryját
+-- Listafüggvényeket a demonstráció kedvéért ne használjuk és segédfüggvényt ne írjunk
+-- A rekurzív hivás során elemeket a környezetből, mert, mivel nincsen kimenet, ezért ez *nincsen reflektálva az eredményben*
+
+getAdminHomes :: Environment -> [String]
+getAdminHomes = undefined
+
+-- A fenti mintákban a környezet továbbadása explicit volt (nekünk kell manuálisan megcsinálni)
+-- Viszont egy absztrakciós réteggel át lehet alakítani implicitté:
+{-
+newtype Reader r a = Reader { runReader :: r -> a }
+-}
+
+-- A Reader monádot fog alkotni, tehát használható a bind művelet
+-- (>>=) :: Reader a  -> (a -> Reader b) -> Reader b
+-- (>>=) :: (r ->  a) -> (a -> r ->   b) -> r ->   b
+{-           ^     |      ^    ^      |     |      ^
+             |     \------/    |      \-----+------/
+             \-----------------+------------/
+
+Az r environment duplikálása és továbbadása mostmár implicit
+Hasonlóan mint Statehez, létezik reader függvény
+-}
+
+homeDirOfR :: String -> Reader Environment (Maybe String)
+homeDirOfR s = reader (homeDirOf s)
+
+getAdminHomesR :: Reader Environment [String]
+getAdminHomesR = undefined
+
+-- A Reader monádnak az alábbi két művelete van
+-- ask: lekérdezi a környezetet
+-- local: lokálisan megváltoztatja
+
+ask' :: Reader r r
+ask' = undefined
+
+local' :: (r -> r) -> Reader r a -> Reader r a
+local' = undefined
+
+-- Írjuk meg a két fenti függvényt ask-al és local-al
+
+homeDirOfRM :: String -> Reader Environment (Maybe String)
+homeDirOfRM = undefined
+
+getAdminHomesRM :: Reader Environment [String]
+getAdminHomesRM = undefined
+
+-- Extra feladatok
+
+-- Definiáljuk a labelWith függvényt readerrel
+
+labelListR :: Num i => [a] -> Reader i [(i,a)]
+labelListR = undefined
+
+-- Definiáljuk a sum függvényt úgy, hogy az olvasási környezetben van a részösszeg
+
+sumTRR :: Num a => [a] -> Reader a a
+sumTRR = undefined
+
+-- Definiáljuk a filterWithIndex függvényt amely index alapján is szűr
+
+filterWithIndexR :: Num i => (i -> a -> Bool) -> [a] -> Reader i [a]
+filterWithIndexR = undefined
+
+-- Definiáljuk a foldl függvényt readerrel
+
+foldlR :: (b -> a -> b) -> [a] -> Reader b b
+foldlR = undefined
+
+-- WRITER
+
+-- Tegyük fel, hogy QA developerek vagyunk és tesztelni akarjuk, hogy a kódunknak milyen code coverage van.
+-- Minden függvényt egy UUID-val trackelünk
+type UUID = String
+
+-- Megváltoztatjuk az aritmetikai függvényeinket, hogy gyűjtsék össze a kimenetben, milyen függvényhívások történtek
+-- Ha az API request "localhost" adjunk vissza True-t különben False-ot. Függetlenül ettől loggoljuk az "apirequest" UUID-t
+apiRequest :: String -> (Bool, [UUID])
+apiRequest = undefined
+
+-- Definiáljuk az alábbi függvényt, amely ha páros számot kap paraméterül akkor a "1.0.0.1" címre küld egy API requestet, ha nem, akkor nem csinál semmit.
+-- Függetlenül ettől loggoljuk a "nameserver" UUID-t.
+-- Eredményül adjuk vissza a szám felét.
+nameserver :: Int -> (Int, [UUID])
+nameserver = undefined
+
+-- A fenti két függvényben a tuple második paramétere, a [UUID] egy kiabsztrahálható réteg.
+-- Ez lesz a Writer monád
+{-
+newtype Writer w a = Writer { runWriter :: (w,a) }
+-}
+-- A writer monádot fog alkotni, ha a w egy monoid
+-- (>>=) :: Writer w a  -> (a -> Writer w b)  -> Writer w b
+-- (>>=) ::       (w,a) -> (a ->       (w,b)) -> (w,b)
+{-                 | \------^           | |       ^ ^
+                   |                    | \-------+-/
+                   \-------------------<+>--------/
+
+Mivel valamilyen <+> módon össze kell a két w-t kombinálni, ezért szükséges a Semigroup megkötés. A Monoid megkötés a return miatt kell:
+return :: a -> Writer w a
+return :: a -> (w,a)
+-}
+
+-- Hasonlóan az előzőekhez, létezik writer függvény
+
+apiRequestW :: String -> Writer [UUID] Bool
+apiRequestW s = writer (apiRequest s)
+
+nameserverW :: Int -> Writer [UUID] Int
+nameserverW = undefined
+
+-- A writernek egy számunkra releváns primitív művelete van:
+
+tell' :: w -> Writer w ()
+tell' = undefined
+
+-- Advancedabb használathoz ld listen és pass
+-- Definiáljuk a fenti két függvény tell segítségével
+
+apiRequestWD :: String -> Writer [UUID] Bool
+apiRequestWD = undefined
+
+nameserverWD :: Int -> Writer [UUID] Int
+nameserverWD = undefined
+
+-- Leggyakrabban lista lesz az írási környezet, de akármilyen Monoid lehet
+-- Tegyük fel, hogy hasító függvényt definiálunk és ehhez az alábbi Hash típust definiáljuk
+
+newtype Hash = Hash { unHash :: Integer }
+  deriving newtype (Show, Eq, Ord, Num, Real, Integral, Enum) -- Inheritálja az Integer instance implementációit
+  deriving Semigroup via Product Integer                      -- Semigroup instance a szorzás alapán
+  deriving Monoid    via Product Integer                      -- UA csak Monoiddal
+
+-- A hasító függvény most prímeket fog összeszorozni modulusz nélkül (nem valami effektív, dont use it in production)
+
+-- Sieve of Erasthothenes
+primes :: Integral a => [a]
+primes = unfoldr (\(x:xs) -> Just (x, filter (\y -> mod y x /= 0) xs)) [2..]
+
+-- Hasítsunk úgy egy integer listát, hogy minden elemével beleindexelünk a prímek listájába és tell-eljük azt a prímet
+hashIntList :: [Int] -> Writer Hash ()
+hashIntList = undefined
+
+-- Hasítsunk tetszőleges hajtogatható tárolót
+hashFoldable :: Foldable f => f Int -> Writer Hash ()
+hashFoldable = undefined
